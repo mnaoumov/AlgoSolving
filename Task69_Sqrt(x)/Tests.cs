0a5ef31d7d96c7ef79c56190ac28f4b3667cb2e2
Michael Naumov <michael.naumov@volia-software.com>
2018-12-04 19:56:02 +0200

    Add tests and implementation

---
+++ Task69_Sqrt(x)/Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            Assert.That(new Solution().MySqrt(4), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void AcceptanceTest2()
+        {
+            Assert.That(new Solution().MySqrt(8), Is.EqualTo(2));
+        }
+
+        [Test]
+        public void Avoid_dividing_by_zero()
+        {
+            Assert.That(() => new Solution().MySqrt(1), Throws.Nothing);
+        }
+    }
+}
