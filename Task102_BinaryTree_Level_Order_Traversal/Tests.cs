e214d15e6ff25363e5f59497a75ff510905d781b
Michael Naumov <michael.naumov@volia-software.com>
2018-12-04 07:24:44 +0200

    Add acceptance test

---
+++ Task102_BinaryTree_Level_Order_Traversal/Tests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task102_BinaryTree_Level_Order_Traversal
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest()
+        {
+            var root = CreateTreeRoot(3, 9, 20, null, null, 15, 7);
+            var expectedResult = new[]
+            {
+                new[] {3},
+                new[] {9, 20},
+                new[] {15, 7},
+            };
+
+            Assert.That(new Solution().LevelOrder(root), Is.EquivalentTo(expectedResult));
+        }
+
+        private static TreeNode CreateTreeRoot(params int?[] values)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
