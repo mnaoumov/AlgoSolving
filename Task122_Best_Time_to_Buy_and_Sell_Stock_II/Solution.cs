98a45a0a8bce36d24f678ca6fb0034a2a9cfb5c5
Michael Naumov <michael.naumov@volia-software.com>
2018-12-07 10:09:52 +0200

    Implement task 122

--- Task122_Best_Time_to_Buy_and_Sell_Stock_II/Solution.cs
+++ Task122_Best_Time_to_Buy_and_Sell_Stock_II/Solution.cs
@@ -1,10 +1,33 @@
 ﻿namespace AlgoSolving.Task122_Best_Time_to_Buy_and_Sell_Stock_II
 {
-    public class Solution {
+    public class Solution
+    {
         public int MaxProfit(int[] prices)
         {
-            // TODO
-            return 0;
+            int result = 0;
+            bool stockBought = false;
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (!stockBought)
+                {
+                    if (i + 1 < prices.Length && prices[i] < prices[i + 1])
+                    {
+                        stockBought = true;
+                        result -= prices[i];
+                    }
+                }
+                else
+                {
+                    if (i + 1 == prices.Length || prices[i] >= prices[i + 1])
+                    {
+                        stockBought = false;
+                        result += prices[i];
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
