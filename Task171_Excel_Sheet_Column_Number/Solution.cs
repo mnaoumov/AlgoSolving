ab52ebae635d1580725bb22430c6927c7ba5c347
Michael Naumov <michael.naumov@volia-software.com>
2018-12-07 07:17:41 +0200

    Add task 171. Excel Sheet Column Number

---
+++ Task171_Excel_Sheet_Column_Number/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace AlgoSolving.Task171_Excel_Sheet_Column_Number
+{
+    public class Solution
+    {
+        public int TitleToNumber(string s)
+        {
+            // TODO
+            return 0;
+        }
+    }
+}
---
+++ Task171_Excel_Sheet_Column_Number/Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task171_Excel_Sheet_Column_Number
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            Assert.That(new Solution().TitleToNumber("A"), Is.EqualTo(1));
+        }
+
+        [Test]
+        public void AcceptanceTest2()
+        {
+            Assert.That(new Solution().TitleToNumber("AB"), Is.EqualTo(28));
+        }
+
+        [Test]
+        public void AcceptanceTest3()
+        {
+            Assert.That(new Solution().TitleToNumber("ZY"), Is.EqualTo(701));
+        }
+    }
+}
