5c844398223cd0a44bc43696051fc373e1429b76
Michael Naumov <michael.naumov@volia-software.com>
2018-12-07 11:02:41 +0200

    Add implementation for 48

--- Task48_Rotate_Image/Solution.cs
+++ Task48_Rotate_Image/Solution.cs
@@ -4,7 +4,33 @@
     {
         public void Rotate(int[,] matrix)
         {
-            // TODO
+            int n = matrix.Length;
+
+            // i,j -> n-1-j,i
+
+            for (int i = 0; i < n / 2; i++)
+            {
+                for (int j = 0; j < n / 2; j++)
+                {
+                    const int cycleLength = 4;
+                    int[] temp = new int[cycleLength];
+                    int iCycle = i;
+                    int jCycle = j;
+                    for (int k = 0; k < cycleLength; k++)
+                    {
+                        temp[k] = matrix[iCycle, jCycle];
+                        iCycle = n - 1 - jCycle;
+                        jCycle = iCycle;
+                    }
+
+                    for (int k = 0; k < cycleLength; k++)
+                    {
+                        matrix[iCycle, jCycle] = temp[(k - 1) % cycleLength];
+                    }
+                }
+            }
+
+
         }
     }
 }
