dddf77fddff316ed2005d76e0196e451181acabc
Michael Naumov <michael.naumov@volia-software.com>
2018-12-07 06:54:55 +0200

    Switch to long temp variable to avoid overflow on checking square

--- Task69_Sqrt(x)/Solution.cs
+++ Task69_Sqrt(x)/Solution.cs
@@ -4,14 +4,14 @@
     {
         public int MySqrt(int x)
         {
-            var result = 1;
+            long result = 1;
 
             while (!(result * result <= x && (result + 1) * (result + 1) > x))
             {
                 result = (result + (x / result)) / 2;
             }
 
-            return result;
+            return (int) result;
         }
     }
 }
