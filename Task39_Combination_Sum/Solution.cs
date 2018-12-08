9cfbb88a5777733b3d9ea860643843dd67e0d9f0
Michael Naumov <michael.naumov@volia-software.com>
2018-12-08 14:50:40 +0200

    Implement #39

--- Task39_Combination_Sum/Solution.cs
+++ Task39_Combination_Sum/Solution.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgoSolving.Task39_Combination_Sum
 {
@@ -6,8 +7,22 @@ namespace AlgoSolving.Task39_Combination_Sum
     {
         public IList<IList<int>> CombinationSum(int[] candidates, int target)
         {
-            // TODO
-            return null;
+            if (target < 0)
+                return new IList<int>[0];
+
+            if (target == 0)
+                return new IList<int>[] {new int[0]};
+
+            List<IList<int>> result = new List<IList<int>>();
+            foreach (var candidate in candidates)
+            {
+                var candidatesStartingFromCurrent = candidates.SkipWhile(c => c != candidate).ToArray();
+                var targetAfterTakingCurrentCandidate = target - candidate;
+                var resultsAfterTakingCurrentCandidate = CombinationSum(candidatesStartingFromCurrent, targetAfterTakingCurrentCandidate);
+                result.AddRange(resultsAfterTakingCurrentCandidate.Select(newResult => new[] {candidate}.Concat(newResult).ToArray()));
+            }
+
+            return result;
         }
     }
 }
