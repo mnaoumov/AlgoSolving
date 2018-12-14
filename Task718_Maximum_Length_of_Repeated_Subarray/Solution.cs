d9b606c3bb8ae6b908aac339137fadfd6c2bb3e9
Michael Naumov <michael.naumov@volia-software.com>
2018-12-14 13:09:04 +0200

    Add 718 description and test

---
+++ Task718_Maximum_Length_of_Repeated_Subarray/Solution.cs
@@ -0,0 +1,11 @@
+﻿namespace AlgoSolving.Task718_Maximum_Length_of_Repeated_Subarray
+{
+    public class Solution
+    {
+        public int FindLength(int[] A, int[] B)
+        {
+            // TODO
+            return 0;
+        }
+    }
+}
---
+++ Task718_Maximum_Length_of_Repeated_Subarray/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task718_Maximum_Length_of_Repeated_Subarray
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            Assert.That(new Solution().FindLength(new[] {1, 2, 3, 2, 1}, new[] {3, 2, 1, 4, 7}), Is.EqualTo(3));
+        }
+    }
+}
