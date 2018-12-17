eb1330731fa6d54f172180f13ee44eba7244e7f5
Michael Naumov <michael.naumov@volia-software.com>
2018-12-17 18:21:29 +0200

    Implement 718

--- Task718_Maximum_Length_of_Repeated_Subarray/Solution.cs
+++ Task718_Maximum_Length_of_Repeated_Subarray/Solution.cs
@@ -1,11 +1,36 @@
-﻿namespace AlgoSolving.Task718_Maximum_Length_of_Repeated_Subarray
+﻿using System;
+
+namespace AlgoSolving.Task718_Maximum_Length_of_Repeated_Subarray
 {
     public class Solution
     {
         public int FindLength(int[] A, int[] B)
         {
-            // TODO
-            return 0;
+            var tailResults = new int?[A.Length, B.Length];
+            return FindLength(A, B, 0, 0, tailResults);
+        }
+
+        private int FindLength(int[] A, int[] B, int indexA, int indexB, int?[,] tailResults)
+        {
+            if (indexA >= A.Length || indexB >= B.Length)
+            {
+                return 0;
+            }
+
+            if (tailResults[indexA, indexB] == null)
+            {
+                tailResults[indexA, indexB] = FindLengthImpl(A, B, indexA, indexB, tailResults);
+            }
+
+            return tailResults[indexA, indexB].Value;
+        }
+
+        private int FindLengthImpl(int[] A, int[] B, int indexA, int indexB, int?[,] tailResults)
+        {
+            var nextA = FindLength(A, B, indexA + 1, indexB, tailResults);
+            var nextB = FindLength(A, B, indexA, indexB + 1, tailResults);
+            var hasMatch = A[indexA] == B[indexB];
+            return Math.Max(Math.Min(nextA, nextB) + (hasMatch ? 1 : 0), Math.Max(nextA, nextB));
         }
     }
 }
