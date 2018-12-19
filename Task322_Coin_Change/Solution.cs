6ab4aeb47357a88fea75300878ea51f0abbc0c11
Michael Naumov <michael.naumov@volia-software.com>
2018-12-19 15:50:55 +0200

    Implement 322

--- Task322_Coin_Change/Solution.cs
+++ Task322_Coin_Change/Solution.cs
@@ -4,7 +4,45 @@
     {
         public int CoinChange(int[] coins, int amount)
         {
-            return 0;
+            var dp = new int?[amount + 1];
+            return CoinChange(coins, amount, dp);
+        }
+
+        private int CoinChange(int[] coins, int amount, int?[] dp)
+        {
+            const int impossibleChange = -1;
+
+            if (amount < 0)
+            {
+                return impossibleChange;
+            }
+
+            if (amount == 0)
+            {
+                return 0;
+            }
+
+            if (dp[amount] != null)
+            {
+                return dp[amount].Value;
+            }
+
+            int result = impossibleChange;
+
+            foreach (var coin in coins)
+            {
+                var subResult = CoinChange(coins, amount - coin, dp);
+                if (subResult != impossibleChange)
+                {
+                    if (result == impossibleChange || result > subResult + 1)
+                    {
+                        result = subResult + 1;
+                    }
+                }
+            }
+
+            dp[amount] = result;
+            return result;
         }
     }
 }
