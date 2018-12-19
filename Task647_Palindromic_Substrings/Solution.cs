12afb65b37af284dc2363b7167568184f8916f5d
Michael Naumov <michael.naumov@volia-software.com>
2018-12-19 02:35:21 +0200

    Implement 647

--- Task647_Palindromic_Substrings/Solution.cs
+++ Task647_Palindromic_Substrings/Solution.cs
@@ -1,10 +1,25 @@
-﻿namespace AlgoSolving.Task647_Palindromic_Substrings
+﻿using System;
+
+namespace AlgoSolving.Task647_Palindromic_Substrings
 {
     public class Solution
     {
         public int CountSubstrings(string s)
         {
-            return 0;
+            var count = 0;
+            for (int doubledMiddlePoint = 0; doubledMiddlePoint < 2 * s.Length - 1; doubledMiddlePoint++)
+            {
+                var left = doubledMiddlePoint / 2;
+                var right = doubledMiddlePoint / 2 + doubledMiddlePoint % 2;
+
+                while (left >= 0 && right < s.Length && s[left] == s[right])
+                {
+                    left--;
+                    right++;
+                    count++;
+                }
+            }
+            return count;
         }
     }
 }
