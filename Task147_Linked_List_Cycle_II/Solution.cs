c1e60b946434d62466c29eb8c9e447c5723e8091
Michael Naumov <michael.naumov@volia-software.com>
2018-12-19 18:42:20 +0200

    Implement 147

--- Task147_Linked_List_Cycle_II/Solution.cs
+++ Task147_Linked_List_Cycle_II/Solution.cs
@@ -17,7 +17,38 @@
     {
         public ListNode DetectCycle(ListNode head)
         {
-            return null;
+            var slowNodeCrawler = head;
+            var fastNodeCrawler = head.next;
+
+            while (fastNodeCrawler != null && fastNodeCrawler.next != null && fastNodeCrawler != slowNodeCrawler)
+            {
+                slowNodeCrawler = slowNodeCrawler.next;
+                fastNodeCrawler = fastNodeCrawler.next.next;
+            }
+
+            if (fastNodeCrawler == null || fastNodeCrawler.next == null)
+            {
+                return null;
+            }
+
+            var cycleFinderAheadCrawler = head;
+
+            do
+            {
+                slowNodeCrawler = slowNodeCrawler.next;
+                fastNodeCrawler = fastNodeCrawler.next.next;
+                cycleFinderAheadCrawler = cycleFinderAheadCrawler.next;
+            } while (fastNodeCrawler != slowNodeCrawler);
+
+            var cycleFinderBehindCrawler = head;
+
+            while (cycleFinderBehindCrawler != cycleFinderAheadCrawler)
+            {
+                cycleFinderBehindCrawler = cycleFinderBehindCrawler.next;
+                cycleFinderAheadCrawler = cycleFinderAheadCrawler.next;
+            }
+
+            return cycleFinderBehindCrawler;
         }
     }
 }
