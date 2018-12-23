8543490045e7f2db065ebed9c4a336100c5eea2c
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 01:58:02 +0200

    377 Implement

--- Task377_Combination_Sum_IV/Solution.cs
+++ Task377_Combination_Sum_IV/Solution.cs
@@ -4,7 +4,23 @@
     {
         public int CombinationSum4(int[] nums, int target)
         {
-            return 0;
+            var partialResults = new int[target + 1];
+            partialResults[0] = 1;
+
+            for (int sum = 0; sum <= target; sum++)
+            {
+                foreach (var num in nums)
+                {
+                    if (num > sum)
+                    {
+                        continue;
+                    }
+
+                    partialResults[sum] += partialResults[sum - num];
+                }
+            }
+
+            return partialResults[target];
         }
     }
 }
