d9723812d20467334fbc790cd2dd712c0ceb9440
Michael Naumov <michael.naumov@volia-software.com>
2018-12-23 23:47:00 +0200

    Update Solutions Anna 147

--- Task147_Linked_List_Cycle_II/Anna.cpp
+++ Task147_Linked_List_Cycle_II/Anna.cpp
@@ -1,33 +1,71 @@
 ﻿// https://pastebin.com/P4LtiZYb
-// TLE?
 
-  ListNode* detectCycle(ListNode* head) {
+    ListNode *detectCycle(ListNode* head) {
         if (!head) return nullptr;
         ListNode* slow = head;
         ListNode* quick = head->next;
-        
+       
+        // find cycle if exists
         while(quick && quick->next && slow != quick) {
             slow = slow->next;
             quick = quick->next->next;
         }
-        
+       
         if (slow != quick) return nullptr;
-        
-        int cycle_len = 0;
-        quick = slow->next;
-        while (quick != slow) {
-            quick = quick->next;
-            ++cycle_len;
-        }
-        
-        slow = head;
+       
+        // calc cycle len and move quick pointer from head on the lenght of the cycle
         quick = head;
-        for (int i = 0; i < cycle_len; ++i) quick = quick->next;
+        auto tmp = slow->next;
         
+        while (tmp != slow) {
+            tmp = tmp->next;
+            quick = quick->next;
+        }
+        quick = quick->next; // tmp was one step ahead of slow
+       
+        // move both pointers till they form a loop and meet at the cycle entry
+        slow = head;
         while(slow != quick) {
             slow = slow->next;
             quick = quick->next;
         }
-        
+       
         return slow;
     }
+
+// https://pastebin.com/RuDP5Sms
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode* head) {
+        if (!head) return nullptr;
+        auto* slow = head;
+        auto* quick = head->next;
+       
+        // find cycle if exists
+        while(quick && quick->next && slow != quick) {
+            slow = slow->next;
+            quick = quick->next->next;
+        }
+       
+        if (slow != quick) return nullptr;
+ 
+        // move both pointers till they form a loop and meet at the cycle entry
+        auto* entry = head;
+        slow = slow->next;
+        while(slow != entry) {
+            slow = slow->next;
+            entry = entry->next;
+        }
+       
+        return slow;
+    }
+};
