024ae28ce021b9670bd7c97e876c0e67c2c75b28
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 00:14:02 +0200

    Implement 416

--- Task416_Partition_Equal_Subset_Sum/Solution.cs
+++ Task416_Partition_Equal_Subset_Sum/Solution.cs
@@ -7,6 +7,31 @@ namespace AlgoSolving.Task416_Partition_Equal_Subset_Sum
     {
         public bool CanPartition(int[] nums)
         {
+            var sum = nums.Sum();
+            if (sum % 2 != 0)
+            {
+                return false;
+            }
+
+            var targetSum = sum / 2;
+
+            var possibleSums = new List<int> {0};
+
+            foreach (var num in nums)
+            {
+                foreach (var possibleSum in possibleSums.ToArray())
+                {
+                    var nextPossibleSum = possibleSum + num;
+                    if (nextPossibleSum == targetSum)
+                    {
+                        return true;
+                    }
+
+                    possibleSums.Add(nextPossibleSum);
+                }
+            }
+
+
             return false;
         }
     }
