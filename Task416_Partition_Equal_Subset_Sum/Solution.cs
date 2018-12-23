a0a9596a31393d61893f3be6dc641223e55f252d
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 00:29:53 +0200

    Replace list with hash set to fix memory

--- Task416_Partition_Equal_Subset_Sum/Solution.cs
+++ Task416_Partition_Equal_Subset_Sum/Solution.cs
@@ -15,7 +15,7 @@ namespace AlgoSolving.Task416_Partition_Equal_Subset_Sum
 
             var targetSum = sum / 2;
 
-            var possibleSums = new List<int> {0};
+            var possibleSums = new HashSet<int> {0};
 
             foreach (var num in nums)
             {
