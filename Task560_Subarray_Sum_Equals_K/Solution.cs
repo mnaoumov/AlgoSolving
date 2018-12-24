98dda7ac43122137a60d9431aa7b989e5dae1f81
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 09:42:03 +0200

    560 Remove caching

--- Task560_Subarray_Sum_Equals_K/Solution.cs
+++ Task560_Subarray_Sum_Equals_K/Solution.cs
@@ -5,15 +5,15 @@
         public int SubarraySum(int[] nums, int k)
         {
             var result = 0;
-            var sums = new int[nums.Length, nums.Length];
 
             for (int i = 0; i < nums.Length; i++)
             {
+                var sum = 0;
                 for (int j = i; j < nums.Length; j++)
                 {
-                    sums[i, j] = (j >= 1 ? sums[i, j - 1] : 0) + nums[j];
+                    sum += nums[j];
 
-                    if (sums[i, j] == k)
+                    if (sum == k)
                     {
                         result++;
                     }
