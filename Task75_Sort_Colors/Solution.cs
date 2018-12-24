e22281323ee34def7e7a61781722b279653c1fdc
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 09:47:17 +0200

    75 add template

---
+++ Task75_Sort_Colors/Solution.cs
@@ -0,0 +1,9 @@
+﻿namespace AlgoSolving.Task75_Sort_Colors
+{
+    public class Solution
+    {
+        public void SortColors(int[] nums)
+        {
+        }
+    }
+}
---
+++ Task75_Sort_Colors/Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task75_Sort_Colors
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            var nums = new[] { 2, 0, 2, 1, 1, 0 };
+            new Solution().SortColors(nums);
+            Assert.That(nums, Is.EqualTo(new[] { 0, 0, 1, 1, 2, 2 }));
+        }
+    }
+}
