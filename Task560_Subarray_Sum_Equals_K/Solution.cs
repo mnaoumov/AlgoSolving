32350d744febc6b0cf195b8a8714313c9382efd7
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 08:46:08 +0200

    560 Brute force with Time Limit Exception

--- Task560_Subarray_Sum_Equals_K/Solution.cs
+++ Task560_Subarray_Sum_Equals_K/Solution.cs
@@ -1,12 +1,29 @@
-﻿using System;
-
-namespace AlgoSolving.Task560_Subarray_Sum_Equals_K
+﻿namespace AlgoSolving.Task560_Subarray_Sum_Equals_K
 {
     public class Solution
     {
         public int SubarraySum(int[] nums, int k)
         {
-            return 0;
+            var result = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i; j < nums.Length; j++)
+                {
+                    var sum = 0;
+                    for (int m = i; m <= j; m++)
+                    {
+                        sum += nums[m];
+                    }
+
+                    if (sum == k)
+                    {
+                        result++;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
