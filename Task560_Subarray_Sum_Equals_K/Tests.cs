ecda11aa3f398a2f255c94cbd73997825305c494
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 08:53:28 +0200

    560 Add failing test Last_element_equaled_to_sum_should_be_counted

--- Task560_Subarray_Sum_Equals_K/Tests.cs
+++ Task560_Subarray_Sum_Equals_K/Tests.cs
@@ -9,5 +9,11 @@ namespace AlgoSolving.Task560_Subarray_Sum_Equals_K
         {
             Assert.That(new Solution().SubarraySum(new[] { 1, 1, 1 }, 2), Is.EqualTo(2));
         }
+
+        [Test]
+        public void Last_element_equaled_to_sum_should_be_counted()
+        {
+            Assert.That(new Solution().SubarraySum(new[] { 1, 2, 3 }, 3), Is.EqualTo(2));
+        }
     }
 }
