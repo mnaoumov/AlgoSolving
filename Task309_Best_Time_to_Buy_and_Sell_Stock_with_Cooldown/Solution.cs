a55f00ed584b340e6d91a18981320027696f6130
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 06:55:35 +0200

    309 implement

--- Task309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown/Solution.cs
+++ Task309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown/Solution.cs
@@ -1,10 +1,25 @@
-﻿namespace AlgoSolving.Task309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown
+﻿using System;
+
+namespace AlgoSolving.Task309_Best_Time_to_Buy_and_Sell_Stock_with_Cooldown
 {
     public class Solution
     {
         public int MaxProfit(int[] prices)
         {
-            return 0;
+            int maxProfitAfterBuying = Int32.MinValue;
+            int maxProfitAfterSelling = 0;
+            int maxProfitAfterSellingTwoDaysAgo = 0;
+            foreach (var price in prices)
+            {
+                int maxProfitAfterSellingOneDayAgo = maxProfitAfterSelling;
+                int maxProfitAfterBuyingOneDayAgo = maxProfitAfterBuying;
+                maxProfitAfterBuying =
+                    Math.Max(maxProfitAfterBuyingOneDayAgo, maxProfitAfterSellingTwoDaysAgo - price);
+                maxProfitAfterSelling = Math.Max(maxProfitAfterSellingOneDayAgo, maxProfitAfterBuying + price);
+                maxProfitAfterSellingTwoDaysAgo = maxProfitAfterSellingOneDayAgo;
+            }
+
+            return maxProfitAfterSelling;
         }
     }
 }
