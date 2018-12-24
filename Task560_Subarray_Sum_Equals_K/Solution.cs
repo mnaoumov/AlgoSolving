a1c14bbdd323f5bdf927e6c33a1bae49f82e54dc
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 08:39:45 +0200

    560 template

---
+++ Task560_Subarray_Sum_Equals_K/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgoSolving.Task560_Subarray_Sum_Equals_K
+{
+    public class Solution
+    {
+        public int SubarraySum(int[] nums, int k)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Task560_Subarray_Sum_Equals_K/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task560_Subarray_Sum_Equals_K
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            Assert.That(new Solution().SubarraySum(new[] {1, 1, 1}, 2), Is.EqualTo(2));
+        }
+    }
+}
