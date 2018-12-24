09be4ececabf5ed2a5313a56d4c07ebb45aa8178
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 03:26:47 +0200

    96 Implement

--- Task96_Unique_Binary_Search_Trees/Solution.cs
+++ Task96_Unique_Binary_Search_Trees/Solution.cs
@@ -1,10 +1,40 @@
-﻿namespace AlgoSolving.Task96_Unique_Binary_Search_Trees
+﻿using System.Collections.Generic;
+
+namespace AlgoSolving.Task96_Unique_Binary_Search_Trees
 {
     public class Solution
     {
         public int NumTrees(int n)
         {
-            return 0;
+            var resultsCache = new Dictionary<int, int>();
+            return NumTrees(n, resultsCache);
+        }
+
+        private int NumTrees(int n, Dictionary<int, int> resultsCache)
+        {
+            if (!resultsCache.ContainsKey(n))
+            {
+                resultsCache[n] = NumTreesInternal(n, resultsCache);
+            }
+
+            return resultsCache[n];
+        }
+
+        private int NumTreesInternal(int n, Dictionary<int, int> resultsCache)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            var result = 0;
+
+            for (int i = 0; i <= n - 1; i++)
+            {
+                result += NumTrees(i, resultsCache) * NumTrees(n - 1 - i, resultsCache);
+            }
+
+            return result;
         }
     }
 }
