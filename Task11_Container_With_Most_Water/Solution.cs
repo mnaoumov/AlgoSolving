62ecebc92cff0ae528f8f3fc47d206fd781b09b4
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 08:27:03 +0200

    11 add template

---
+++ Task11_Container_With_Most_Water/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task11_Container_With_Most_Water
+{
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            return 0;
+        }
+    }
+}
