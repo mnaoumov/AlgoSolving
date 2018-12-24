77d050ee920d3f945395b5ebde463f65babf7edd
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 09:57:55 +0200

    75 implement

--- Task75_Sort_Colors/Solution.cs
+++ Task75_Sort_Colors/Solution.cs
@@ -4,6 +4,24 @@
     {
         public void SortColors(int[] nums)
         {
+            var counts = new int[3];
+            foreach (var num in nums)
+            {
+                counts[num]++;
+            }
+
+            var lastColor = 0;
+            var lastColorIndexShift = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i >= counts[lastColor] + lastColorIndexShift)
+                {
+                    lastColor++;
+                    lastColorIndexShift = i;
+                }
+
+                nums[i] = lastColor;
+            }
         }
     }
 }
