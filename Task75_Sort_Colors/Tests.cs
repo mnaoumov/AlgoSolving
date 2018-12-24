f973ff18d4574cef7f2e271b3064e8c6cf4bc48b
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 09:59:00 +0200

    75 Add failing test Work_properly_for_single_last_color

--- Task75_Sort_Colors/Tests.cs
+++ Task75_Sort_Colors/Tests.cs
@@ -11,5 +11,13 @@ namespace AlgoSolving.Task75_Sort_Colors
             new Solution().SortColors(nums);
             Assert.That(nums, Is.EqualTo(new[] { 0, 0, 1, 1, 2, 2 }));
         }
+
+        [Test]
+        public void Work_properly_for_single_last_color()
+        {
+            var nums = new[] { 2 };
+            new Solution().SortColors(nums);
+            Assert.That(nums, Is.EqualTo(new[] { 2 }));
+        }
     }
 }
