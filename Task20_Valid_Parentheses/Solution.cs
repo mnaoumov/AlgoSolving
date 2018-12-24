edc9ff2461b1d13d2560aa69cf95894b00cef79b
Michael Naumov <michael.naumov@volia-software.com>
2018-12-24 08:21:04 +0200

    20 implement

--- Task20_Valid_Parentheses/Solution.cs
+++ Task20_Valid_Parentheses/Solution.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 
 namespace AlgoSolving.Task20_Valid_Parentheses
 {
@@ -6,7 +6,54 @@ namespace AlgoSolving.Task20_Valid_Parentheses
     {
         public bool IsValid(string s)
         {
-            return false;
+            var openBrackets = new Dictionary<char, BracketType>
+            {
+                ['('] = BracketType.Round,
+                ['['] = BracketType.Square,
+                ['{'] = BracketType.Curly
+            };
+
+            var closeBrackets = new Dictionary<char, BracketType>
+            {
+                [')'] = BracketType.Round,
+                [']'] = BracketType.Square,
+                ['}'] = BracketType.Curly
+            };
+
+            var brackets = new Stack<BracketType>();
+
+            foreach (var symbol in s)
+            {
+                if (openBrackets.ContainsKey(symbol))
+                {
+                    brackets.Push(openBrackets[symbol]);
+                }
+                else if (closeBrackets.ContainsKey(symbol))
+                {
+                    if (brackets.Count == 0)
+                    {
+                        return false;
+                    }
+
+                    var closeBracketType = closeBrackets[symbol];
+                    var lastBracketType = brackets.Pop();
+
+                    if (closeBracketType != lastBracketType)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return brackets.Count == 0;
+        }
+
+        private enum BracketType
+        {
+            Round,
+            Square,
+            Curly
+
         }
     }
 }
