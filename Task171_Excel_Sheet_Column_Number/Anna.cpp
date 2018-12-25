e1e86eedd6c4cef2f4ee5dafca12aa22b873913b
Michael Naumov <michael.naumov@volia-software.com>
2018-12-25 19:07:00 +0200

    Remove Anna solutions as I can't ensure they are up-to-date

--- Task171_Excel_Sheet_Column_Number/Anna.cpp
+++
@@ -1,9 +0,0 @@
-﻿// https://pastebin.com/TYUGcfjB
-
- int titleToNumber(string s) {
-    const int kBase = 26;
-    int res = 0;
-    for (auto c : s)
-        res = res * kBase + (c - 'A' + 1);
-    return res;
-}
