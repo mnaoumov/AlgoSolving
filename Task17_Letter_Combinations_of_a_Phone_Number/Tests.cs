0453563921874d5c650cdaf602ae97081820724b
Michael Naumov <michael.naumov@volia-software.com>
2018-12-26 09:31:12 +0200

    17 Add failing Empty_string_returns_empty_list

--- Task17_Letter_Combinations_of_a_Phone_Number/Tests.cs
+++ Task17_Letter_Combinations_of_a_Phone_Number/Tests.cs
@@ -10,5 +10,11 @@ namespace AlgoSolving.Task17_Letter_Combinations_of_a_Phone_Number
             Assert.That(new Solution().LetterCombinations("23"),
                 Is.EquivalentTo(new[] { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" }));
         }
+
+        [Test]
+        public void Empty_string_returns_empty_list()
+        {
+            Assert.That(new Solution().LetterCombinations(""), Is.Empty);
+        }
     }
 }
