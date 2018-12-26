b771467e5d4a24896e206819dd10bbc16ef16109
Michael Naumov <michael.naumov@volia-software.com>
2018-12-26 12:25:32 +0200

    105 Implement

--- Task105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Solution.cs
+++ Task105_Construct_Binary_Tree_from_Preorder_and_Inorder_Traversal/Solution.cs
@@ -4,7 +4,31 @@
     {
         public TreeNode BuildTree(int[] preorder, int[] inorder)
         {
-            return null;
+            return BuildTree(preorder, inorder, 0, 0, preorder.Length);
+        }
+
+        private TreeNode BuildTree(int[] preorder, int[] inorder, int preorderIndexFrom, int inorderIndexFrom, int childrenCount)
+        {
+            if (childrenCount == 0)
+            {
+                return null;
+            }
+
+            var root = new TreeNode(preorder[preorderIndexFrom]);
+
+            int leftNodeInorderIndex = inorderIndexFrom;
+            while (inorder[leftNodeInorderIndex] != root.val)
+            {
+                leftNodeInorderIndex++;
+            }
+
+            var leftNodeChildrenCount = leftNodeInorderIndex - inorderIndexFrom;
+            root.left = BuildTree(preorder, inorder, preorderIndexFrom + 1, inorderIndexFrom,
+                leftNodeChildrenCount);
+            root.right = BuildTree(preorder, inorder, preorderIndexFrom + leftNodeChildrenCount + 1, leftNodeInorderIndex + 1,
+                childrenCount - leftNodeChildrenCount - 1);
+
+            return root;
         }
     }
 }
