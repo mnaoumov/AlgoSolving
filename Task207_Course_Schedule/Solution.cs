b1564c03fabb539498c153b8ccfa340ff40ed657
Michael Naumov <michael.naumov@volia-software.com>
2018-12-26 13:19:51 +0200

    207 Implement

--- Task207_Course_Schedule/Solution.cs
+++ Task207_Course_Schedule/Solution.cs
@@ -1,10 +1,46 @@
-﻿namespace AlgoSolving.Task207_Course_Schedule
+﻿using System.Collections.Generic;
+
+namespace AlgoSolving.Task207_Course_Schedule
 {
     public class Solution
     {
         public bool CanFinish(int numCourses, int[,] prerequisites)
         {
-            return false;
+            var dependencies = new HashSet<int>[numCourses];
+
+            for (int course = 0; course < numCourses; course++)
+            {
+                dependencies[course] = new HashSet<int>();
+            }
+
+            for (int i = 0; i < prerequisites.GetLength(0); i++)
+            {
+                dependencies[prerequisites[i, 0]].Add(prerequisites[i, 1]);
+            }
+
+            var checkedDependencies = new bool[numCourses, numCourses];
+
+            for (int course = 0; course < numCourses; course++)
+            {
+                var prerequisiteSet = new HashSet<int> { course };
+                var courseToCheck = course;
+
+                foreach (var dependency in dependencies[courseToCheck])
+                {
+                    if (!checkedDependencies[courseToCheck, dependency])
+                    {
+                        if (prerequisiteSet.Contains(dependency))
+                        {
+                            return false;
+                        }
+                        checkedDependencies[courseToCheck, dependency] = true;
+                        prerequisiteSet.Add(dependency);
+                        courseToCheck = dependency;
+                    }
+                }
+            }
+
+            return true;
         }
     }
 }
