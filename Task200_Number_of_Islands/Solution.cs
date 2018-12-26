cc5105a5df67b81f223a7de127c025a5fa955349
Michael Naumov <michael.naumov@volia-software.com>
2018-12-26 09:50:54 +0200

    200 Implement

--- Task200_Number_of_Islands/Solution.cs
+++ Task200_Number_of_Islands/Solution.cs
@@ -4,7 +4,45 @@
     {
         public int NumIslands(char[,] grid)
         {
-            return 0;
+            var result = 0;
+            var m = grid.GetLength(0);
+            var n = grid.GetLength(1);
+            int[,] islandNumber = new int[m, n];
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    const char land = '1';
+                    if (grid[i, j] == land)
+                    {
+                        result++;
+
+                        var isLandOnTop = i > 0 && grid[i - 1, j] == land;
+
+                        if (isLandOnTop)
+                        {
+                            result--;
+                        }
+
+                        var isLandOnLeft = j > 0 && grid[i, j - 1] == land;
+
+                        if (isLandOnLeft)
+                        {
+                            result--;
+                        }
+
+                        if (isLandOnLeft && isLandOnTop && islandNumber[i - 1, j] == islandNumber[i, j - 1])
+                        {
+                            result++;
+                        }
+
+                        islandNumber[i, j] = result;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
