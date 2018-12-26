9352122d8119897350710485e8dd7a7b0204d9a2
Michael Naumov <michael.naumov@volia-software.com>
2018-12-26 18:16:32 +0200

    56 Use custom IntervalComparer for tests

--- Task56_Merge_Intervarls/Tests.cs
+++ Task56_Merge_Intervarls/Tests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using NUnit.Framework;
 
 namespace AlgoSolving.Task56_Merge_Intervarls
 {
@@ -13,13 +14,12 @@ namespace AlgoSolving.Task56_Merge_Intervarls
                 new Interval(2, 6),
                 new Interval(8, 10),
                 new Interval(15, 18)
-            }), Is.EqualTo(new[]
+            }), Is.EquivalentTo(new[]
             {
                 new Interval(1, 6),
                 new Interval(8, 10),
-                new Interval(8, 10),
                 new Interval(15, 18)
-            }));
+            }).Using<Interval, Interval>(IntervalComparer));
         }
 
         [Test]
@@ -29,10 +29,15 @@ namespace AlgoSolving.Task56_Merge_Intervarls
             {
                 new Interval(1, 4),
                 new Interval(4, 5)
-            }), Is.EqualTo(new[]
+            }), Is.EquivalentTo(new[]
             {
                 new Interval(1, 5)
-            }));
+            }).Using<Interval, Interval>(IntervalComparer));
+        }
+
+        private static bool IntervalComparer(Interval interval1, Interval interval2)
+        {
+            return interval1.start == interval2.start && interval1.end == interval2.end;
         }
     }
 }
