59c626ccf29ea44837b2024957288b0742f9f698
Michael Naumov <michael.naumov@volia-software.com>
2018-12-26 09:29:09 +0200

    17 Fix test for not checking order

--- Task17_Letter_Combinations_of_a_Phone_Number/Tests.cs
+++ Task17_Letter_Combinations_of_a_Phone_Number/Tests.cs
@@ -8,7 +8,7 @@ namespace AlgoSolving.Task17_Letter_Combinations_of_a_Phone_Number
         public void AcceptanceTest1()
         {
             Assert.That(new Solution().LetterCombinations("23"),
-                Is.EqualTo(new[] { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" }));
+                Is.EquivalentTo(new[] { "ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf" }));
         }
     }
 }
