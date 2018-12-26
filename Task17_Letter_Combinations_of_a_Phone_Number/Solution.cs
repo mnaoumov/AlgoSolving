0be26725c7baed8e6ea27fb22db926985f9393ec
Michael Naumov <michael.naumov@volia-software.com>
2018-12-26 09:28:52 +0200

    17 Implement

--- Task17_Letter_Combinations_of_a_Phone_Number/Solution.cs
+++ Task17_Letter_Combinations_of_a_Phone_Number/Solution.cs
@@ -6,7 +6,35 @@ namespace AlgoSolving.Task17_Letter_Combinations_of_a_Phone_Number
     {
         public IList<string> LetterCombinations(string digits)
         {
-            return null;
+            var mapping = new Dictionary<char, string>
+            {
+                ['2'] = "abc",
+                ['3'] = "def",
+                ['4'] = "ghi",
+                ['5'] = "jkl",
+                ['6'] = "mno",
+                ['7'] = "pqrs",
+                ['8'] = "tuv",
+                ['9'] = "wxyz"
+            };
+
+            var combinations = new List<string> { "" };
+
+            foreach (var digit in digits)
+            {
+                var nextCombinations = new List<string>();
+                foreach (var letter in mapping[digit])
+                {
+                    foreach (var combination in combinations)
+                    {
+                        nextCombinations.Add(combination + letter);
+                    }
+                }
+
+                combinations = nextCombinations;
+            }
+
+            return combinations;
         }
     }
 }
