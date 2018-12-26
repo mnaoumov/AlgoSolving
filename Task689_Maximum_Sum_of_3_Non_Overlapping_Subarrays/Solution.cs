af94e86995a8fb6c1f89e61feb5e205ee8aedb5d
Michael Naumov <michael.naumov@volia-software.com>
2018-12-26 20:40:59 +0200

    689 Add template

---
+++ Task689_Maximum_Sum_of_3_Non_Overlapping_Subarrays/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task689_Maximum_Sum_of_3_Non_Overlapping_Subarrays
+{
+    public class Solution
+    {
+        public int[] MaxSumOfThreeSubarrays(int[] nums, int k)
+        {
+            return null;
+        }
+    }
+}
---
+++ Task689_Maximum_Sum_of_3_Non_Overlapping_Subarrays/Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task689_Maximum_Sum_of_3_Non_Overlapping_Subarrays
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            Assert.That(new Solution().MaxSumOfThreeSubarrays(new[] {1, 2, 1, 2, 6, 7, 5, 1}, 2),
+                Is.EqualTo(new[] {0, 3, 5}));
+        }
+    }
+}
