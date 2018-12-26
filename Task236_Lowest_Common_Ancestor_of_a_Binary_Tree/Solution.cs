05bfc0b555b17f1e1577f4db3fa1954ee41c76df
Michael Naumov <michael.naumov@volia-software.com>
2018-12-26 18:44:42 +0200

    236 Implement

--- Task236_Lowest_Common_Ancestor_of_a_Binary_Tree/Solution.cs
+++ Task236_Lowest_Common_Ancestor_of_a_Binary_Tree/Solution.cs
@@ -1,10 +1,17 @@
-﻿namespace AlgoSolving.Task236_Lowest_Common_Ancestor_of_a_Binary_Tree
+﻿using System.Linq;
+
+namespace AlgoSolving.Task236_Lowest_Common_Ancestor_of_a_Binary_Tree
 {
     public class Solution
     {
         public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
         {
-            return null;
+            if (new[] { null, p, q }.Contains(root))
+            {
+                return root;
+            }
+
+            return LowestCommonAncestor(root.left, p, q) ?? LowestCommonAncestor(root.right, p, q) ?? root;
         }
     }
 }
