536139a85b9ecac0581b29e970cb2b76424c6d8a
Michael Naumov <michael.naumov@volia-software.com>
2018-12-31 22:26:24 +0200

    34 Refactor

--- Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.cs
+++ Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.cs
@@ -8,16 +8,32 @@ namespace AlgoSolving.Task34_Find_First_and_Last_Position_of_Element_in_Sorted_A
         {
             const int notFoundIndex = -1;
 
-            int firstIndex = notFoundIndex;
-            var lastIndex = notFoundIndex;
+            var firstIndex = FindLast(nums, x => x < target) + 1;
+            var lastIndex = FindLast(nums, x => x <= target);
 
+            if (nums[firstIndex] != target)
+            {
+                firstIndex = notFoundIndex;
+            }
+
+            if (nums[lastIndex] != target)
+            {
+                lastIndex = notFoundIndex;
+            }
+
+            return new[] { firstIndex, lastIndex };
+        }
+
+        private static int FindLast(int[] nums, Func<int, bool> condition)
+        {
             var left = 0;
             var right = nums.Length;
 
             while (left < right)
             {
                 var middle = (left + right) / 2;
-                if (nums[middle] < target)
+
+                if (condition(nums[middle]))
                 {
                     left = middle + 1;
                 }
@@ -27,33 +43,7 @@ namespace AlgoSolving.Task34_Find_First_and_Last_Position_of_Element_in_Sorted_A
                 }
             }
 
-            if (nums[left + 1] == target)
-            {
-                firstIndex = left + 1;
-            }
-
-            left = 0;
-            right = nums.Length;
-
-            while (left < right)
-            {
-                var middle = (left + right) / 2;
-                if (nums[middle] <= target)
-                {
-                    left = middle + 1;
-                }
-                else
-                {
-                    right = middle - 1;
-                }
-            }
-
-            if (nums[left] == target)
-            {
-                lastIndex = left;
-            }
-
-            return new[] { firstIndex, lastIndex };
+            return left;
         }
     }
 }
