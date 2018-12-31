3853878a4259eb4dd185a044090a043a3a9251f9
Michael Naumov <michael.naumov@volia-software.com>
2018-12-31 21:46:05 +0200

    34 Implement

--- Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.cs
+++ Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.cs
@@ -1,10 +1,42 @@
-﻿namespace AlgoSolving.Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array
+﻿using System;
+
+namespace AlgoSolving.Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array
 {
     public class Solution
     {
         public int[] SearchRange(int[] nums, int target)
         {
-            return new int[] { };
+            var firstIndex = SearchRange(nums, target, x => x < target);
+            var lastIndex = SearchRange(nums, target, x => x <= target);
+
+            return new[] { firstIndex, lastIndex };
+        }
+
+        private static int SearchRange(int[] nums, int target, Func<int, bool> leftPartCondition)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while (left < right)
+            {
+                var middle = (left + right) / 2;
+                if (leftPartCondition(nums[middle]))
+                {
+                    right = middle;
+                }
+                else
+                {
+                    left = middle;
+                }
+            }
+
+            if (nums[left] == target)
+            {
+                return left;
+            }
+
+            const int notFoundIndex = -1;
+            return notFoundIndex;
         }
     }
 }
