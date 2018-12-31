aeb73b697131a8e289b2a2779a42f6a2ad68ef0f
Michael Naumov <michael.naumov@volia-software.com>
2018-12-31 19:20:21 +0200

    34 Add template

---
+++ Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array
+{
+    public class Solution
+    {
+        public int[] SearchRange(int[] nums, int target)
+        {
+            return new int[] { };
+        }
+    }
+}
---
+++ Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            Assert.That(new Solution().SearchRange(new[] { 5, 7, 7, 8, 8, 10 }, 8), Is.EqualTo(new[] { 3, 4 }));
+        }
+
+        [Test]
+        public void AcceptanceTest2()
+        {
+            Assert.That(new Solution().SearchRange(new[] { 5, 7, 7, 8, 8, 10 }, 6), Is.EqualTo(new[] { -1, -1 }));
+        }
+    }
+}
