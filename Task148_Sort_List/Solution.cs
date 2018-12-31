79669a4e51031722f79fa78e6e3a30eb65677e86
Michael Naumov <michael.naumov@volia-software.com>
2018-12-31 18:42:51 +0200

    148 Add template

---
+++ Task148_Sort_List/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task148_Sort_List
+{
+    public class Solution
+    {
+        public ListNode SortList(ListNode head)
+        {
+            return null;
+        }
+    }
+}
