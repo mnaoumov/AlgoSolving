972b2cbde533063ad9330bde490b4374f9ce06fa
Michael Naumov <michael.naumov@volia-software.com>
2018-12-31 18:19:57 +0200

    19 Add template

---
+++ Task19_Remove_Nth_Node_From_End_of_List/ListNode.cs
@@ -0,0 +1,15 @@
+﻿namespace AlgoSolving.Task19_Remove_Nth_Node_From_End_of_List
+{
+    /// <summary>
+    /// Definition for singly-linked list.
+    /// </summary>
+    public class ListNode {
+        public int val;
+        public ListNode next;
+
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+}
---
+++ Task19_Remove_Nth_Node_From_End_of_List/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task19_Remove_Nth_Node_From_End_of_List
+{
+    public class Solution
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            return null;
+        }
+    }
+}
