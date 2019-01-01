0c6e2d81c1fd861a907466db4762f34cc7fd3d16
Michael Naumov <michael.naumov@volia-software.com>
2019-01-01 04:11:12 +0200

    34 Remove using

--- Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.cs
+++ Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace AlgoSolving.Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array
+﻿namespace AlgoSolving.Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array
 {
     public class Solution
     {
