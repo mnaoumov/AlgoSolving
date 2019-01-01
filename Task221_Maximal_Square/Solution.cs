7ff6a4f139962d5106828583015e3a867e21be38
Michael Naumov <michael.naumov@volia-software.com>
2019-01-01 06:20:15 +0200

    221 Implement

--- Task221_Maximal_Square/Solution.cs
+++ Task221_Maximal_Square/Solution.cs
@@ -1,10 +1,44 @@
-﻿namespace AlgoSolving.Task221_Maximal_Square
+﻿using System.Linq;
+
+namespace AlgoSolving.Task221_Maximal_Square
 {
     public class Solution
     {
         public int MaximalSquare(char[,] matrix)
         {
-            return 0;
+            const char squareSymbol = '1';
+
+            var m = matrix.GetLength(0);
+            var n = matrix.GetLength(1);
+
+            int[,] leftTopSquareSizes = new int[m + 1, n + 1];
+            var maxSize = 0;
+
+            for (int i = m - 1; i >= 0 ; i--)
+            {
+                for (int j = n - 1; j >= 0; j--)
+                {
+                    if (matrix[i, j] != squareSymbol)
+                    {
+                        leftTopSquareSizes[i, j] = 0;
+                        continue;
+                    }
+
+                    var previousSquareSize = leftTopSquareSizes[i + 1, j + 1];
+
+                    var areLeftAndTopSidesFull =
+                        Enumerable.Range(i + 1, previousSquareSize).All(k => matrix[k, j] == squareSymbol) ||
+                        Enumerable.Range(j + 1, previousSquareSize).All(k => matrix[i, k] == squareSymbol);
+
+                    leftTopSquareSizes[i, j] = previousSquareSize + (areLeftAndTopSidesFull ? 1 : 0);
+                    if (leftTopSquareSizes[i, j] > maxSize)
+                    {
+                        maxSize = leftTopSquareSizes[i, j];
+                    }
+                }
+            }
+
+            return maxSize * maxSize;
         }
     }
 }
