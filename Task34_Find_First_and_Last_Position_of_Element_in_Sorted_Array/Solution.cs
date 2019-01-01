25ce003c3783b70f60ae4f9c80aebbbd50a9c14f
Michael Naumov <michael.naumov@volia-software.com>
2019-01-01 03:51:20 +0200

    34 Rewrite from scratch to get firstIndex correctly

--- Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.cs
+++ Task34_Find_First_and_Last_Position_of_Element_in_Sorted_Array/Solution.cs
@@ -8,48 +8,39 @@ namespace AlgoSolving.Task34_Find_First_and_Last_Position_of_Element_in_Sorted_A
         {
             const int notFoundIndex = -1;
 
-            var firstIndex = FindLast(nums, x => x < target) + 1;
-            var lastIndex = FindLast(nums, x => x <= target);
-
-            if (firstIndex >= nums.Length || nums[firstIndex] != target)
+            if (nums.Length == 0 || nums[0] > target || nums[nums.Length - 1] < target)
             {
-                firstIndex = notFoundIndex;
+                return new[] { notFoundIndex, notFoundIndex };
             }
 
-            if (lastIndex >= nums.Length || nums[lastIndex] != target)
-            {
-                lastIndex = notFoundIndex;
-            }
-
-            return new[] { firstIndex, lastIndex };
-        }
-
-        private static int FindLast(int[] nums, Func<int, bool> condition)
-        {
             var left = 0;
             var right = nums.Length;
 
             while (right - left > 1)
             {
                 var middle = (left + right) / 2;
+                var value = nums[middle];
 
-                if (condition(nums[middle]))
+                if (value < target)
                 {
                     left = middle + 1;
                 }
-                else
+                else if (value > target)
                 {
                     right = middle - 1;
                 }
+                else
+                {
+                    right = middle;
+                }
             }
 
-            if (!condition(left))
+            if (right < nums.Length && nums[right] == target)
             {
-                const int notFoundIndex = -1;;
-                return notFoundIndex;
+                return new[] { right, right };
             }
 
-            return left;
+            return new[] { notFoundIndex, notFoundIndex };
         }
     }
 }
