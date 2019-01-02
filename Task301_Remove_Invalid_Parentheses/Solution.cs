17490dabd7a067cbb4baf0aa93a1305b1093fb18
Michael Naumov <michael.naumov@volia-software.com>
2019-01-02 05:12:11 +0200

    301 Implement removal of closed brackets

--- Task301_Remove_Invalid_Parentheses/Solution.cs
+++ Task301_Remove_Invalid_Parentheses/Solution.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgoSolving.Task301_Remove_Invalid_Parentheses
 {
@@ -6,7 +8,66 @@ namespace AlgoSolving.Task301_Remove_Invalid_Parentheses
     {
         public IList<string> RemoveInvalidParentheses(string s)
         {
-            return null;
+            return RemoveInvalidParentheses(s, 0).ToList();
+        }
+
+        private IList<string> RemoveInvalidParentheses(string s, int start)
+        {
+            var results = new List<string>();
+            var balance = 0;
+            var openBracketIndices = new List<int>();
+            var closedBracketIndices = new List<int>();
+            var balances = new int[s.Length];
+            for (int i = start; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    balance++;
+                    openBracketIndices.Add(i);
+                }
+                else if (s[i] == ')')
+                {
+                    balance--;
+                    closedBracketIndices.Add(i);
+                }
+
+                if (balance < 0)
+                {
+                    var suffixResults = RemoveInvalidParentheses(s, i + 1);
+                    var prefixes = new HashSet<string>();
+                    var substring = s.Substring(start, i - start + 1);
+                    foreach (var closedBracketIndex in closedBracketIndices)
+                    {
+                        var prefix = substring.Remove(closedBracketIndex - start, 1);
+                        prefixes.Add(prefix);
+                    }
+
+                    foreach (var prefix in prefixes)
+                    {
+                        foreach (var suffixResult in suffixResults)
+                        {
+                            results.Add(prefix + suffixResult);
+                        }
+                    }
+
+                    return results;
+                }
+
+                balances[i] = balance;
+            }
+
+            if (balance == 0)
+            {
+                return new[] { s.Substring(start, s.Length - start) };
+            }
+
+            return RemoveOpenBrackets(results, start, openBracketIndices, balances, balance);
+        }
+
+        private IList<string> RemoveOpenBrackets(List<string> results, int start, List<int> openBracketIndices,
+            int[] balances, int balance)
+        {
+            return new List<string>{ "" };
         }
     }
 }
