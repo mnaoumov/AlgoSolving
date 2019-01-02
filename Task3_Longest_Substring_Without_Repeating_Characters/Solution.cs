4c5e8276513e6dbe5a4dfa9dce4e2d85e957a44c
Michael Naumov <michael.naumov@volia-software.com>
2019-01-02 04:06:58 +0200

    3 Implement

--- Task3_Longest_Substring_Without_Repeating_Characters/Solution.cs
+++ Task3_Longest_Substring_Without_Repeating_Characters/Solution.cs
@@ -1,10 +1,26 @@
-﻿namespace AlgoSolving.Task3_Longest_Substring_Without_Repeating_Characters
+﻿using System;
+
+namespace AlgoSolving.Task3_Longest_Substring_Without_Repeating_Characters
 {
     public class Solution
     {
         public int LengthOfLongestSubstring(string s)
         {
-            return 0;
+            var result = 1;
+            var previousMaxSuffixLength = 1;
+            for (int i = s.Length - 2; i >= 0; i--)
+            {
+                int maxSuffixLength = 1;
+
+                while (maxSuffixLength <= previousMaxSuffixLength && s[i] != s[i + maxSuffixLength])
+                {
+                    maxSuffixLength++;
+                }
+
+                previousMaxSuffixLength = maxSuffixLength;
+                result = Math.Max(result, maxSuffixLength);
+            }
+            return result;
         }
     }
 }
