afc1552d8b95fdb72f94a1ad09dfd5a516f72918
Michael Naumov <michael.naumov@volia-software.com>
2019-01-08 05:25:56 +0200

    297 Fix test to use not null node

--- Task297_Serialize_and_Deserialize_Binary_Tree/Tests.cs
+++ Task297_Serialize_and_Deserialize_Binary_Tree/Tests.cs
@@ -7,7 +7,17 @@ namespace AlgoSolving.Task297_Serialize_and_Deserialize_Binary_Tree
         [Test]
         public void AcceptanceTest1()
         {
-            TreeNode root = null;
+            TreeNode root = new TreeNode(1)
+            {
+                left = new TreeNode(2)
+                {
+                    left = new TreeNode(4)
+                },
+                right = new TreeNode(3)
+                {
+                    right = new TreeNode(5)
+                }
+            };
             var serialized = new Codec().serialize(root);
             var deserialized = new Codec().deserialize(serialized);
             Assert.That(deserialized, Is.EqualTo(root).Using(new TreeNodeEqualityComparer()));
