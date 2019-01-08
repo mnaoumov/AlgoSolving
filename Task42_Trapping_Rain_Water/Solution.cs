d5bea5ec648c2d8565c676cf46e7839c1a3be9ee
Michael Naumov <michael.naumov@volia-software.com>
2019-01-08 05:57:34 +0200

    42 Add template

---
+++ Task42_Trapping_Rain_Water/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task42_Trapping_Rain_Water
+{
+    public class Solution
+    {
+        public int Trap(int[] height)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Task42_Trapping_Rain_Water/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task42_Trapping_Rain_Water
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            Assert.That(new Solution().Trap(new[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }), Is.EqualTo(6));
+        }
+    }
+}
