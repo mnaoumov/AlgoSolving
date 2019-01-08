637e36a50a0dc14758a564abab4410569d526039
Michael Naumov <michael.naumov@volia-software.com>
2019-01-08 21:01:45 +0200

    312 Add template

---
+++ Task312_Burst_Balloons/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task312_Burst_Balloons
+{
+    public class Solution
+    {
+        public int MaxCoins(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Task312_Burst_Balloons/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task312_Burst_Balloons
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            Assert.That(new Solution().MaxCoins(new[] { 3, 1, 5, 8 }), Is.EqualTo(167));
+        }
+    }
+}
