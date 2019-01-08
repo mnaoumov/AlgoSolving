b0e4527f58f24bbabbb8449dfd642028690d0f8d
Michael Naumov <michael.naumov@volia-software.com>
2019-01-08 21:09:05 +0200

    312 Implement

--- Task312_Burst_Balloons/Solution.cs
+++ Task312_Burst_Balloons/Solution.cs
@@ -1,10 +1,37 @@
-﻿namespace AlgoSolving.Task312_Burst_Balloons
+﻿using System.Collections.Generic;
+
+namespace AlgoSolving.Task312_Burst_Balloons
 {
     public class Solution
     {
         public int MaxCoins(int[] nums)
         {
-            return 0;
+            return MaxCoins(new List<int>(nums));
+        }
+
+        private int MaxCoins(List<int> nums)
+        {
+            if (nums.Count == 0)
+            {
+                return 0;
+            }
+
+            var max = int.MinValue;
+            for (int i = 0; i < nums.Count; i++)
+            {
+                var current = nums[i];
+                var left = i - 1 >= 0 ? nums[i - 1] : 1;
+                var right = (i + 1 < nums.Count ? nums[i + 1] : 1);
+                nums.RemoveAt(i);
+                var candidate = left * current * right + MaxCoins(nums);
+                if (candidate > max)
+                {
+                    max = candidate;
+                }
+                nums.Insert(i, current);
+            }
+
+            return max;
         }
     }
 }
