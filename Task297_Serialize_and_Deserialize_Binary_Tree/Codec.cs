3d1b41d017190dc6f4801961d373a13450132882
Michael Naumov <michael.naumov@volia-software.com>
2019-01-08 05:24:08 +0200

    297 Add template

---
+++ Task297_Serialize_and_Deserialize_Binary_Tree/Codec.cs
@@ -0,0 +1,25 @@
+﻿namespace AlgoSolving.Task297_Serialize_and_Deserialize_Binary_Tree
+{
+    public class Codec
+    {
+        /// <summary>
+        /// Encodes a tree to a single string.
+        /// </summary>
+        /// <param name="root"></param>
+        /// <returns></returns>
+        public string serialize(TreeNode root)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Decodes your encoded data to tree.
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public TreeNode deserialize(string data)
+        {
+            return null;
+        }
+    }
+}
---
+++ Task297_Serialize_and_Deserialize_Binary_Tree/TreeNode.cs
@@ -0,0 +1,17 @@
+﻿namespace AlgoSolving.Task297_Serialize_and_Deserialize_Binary_Tree
+{
+    /// <summary>
+    /// Definition for a binary tree node.
+    /// </summary>
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+}
