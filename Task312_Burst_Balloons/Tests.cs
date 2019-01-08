04b31e6c2174ee7bbf382be464698f10e795b0e9
Michael Naumov <michael.naumov@volia-software.com>
2019-01-08 21:24:31 +0200

    312 Add timeout test

--- Task312_Burst_Balloons/Tests.cs
+++ Task312_Burst_Balloons/Tests.cs
@@ -9,5 +9,12 @@ namespace AlgoSolving.Task312_Burst_Balloons
         {
             Assert.That(new Solution().MaxCoins(new[] { 3, 1, 5, 8 }), Is.EqualTo(167));
         }
+
+        [Test]
+        [Timeout(1000)]
+        public void Timeout()
+        {
+            new Solution().MaxCoins(new[] { 7, 9, 8, 0, 7, 1, 3, 5, 5, 2, 3, 3 });
+        }
     }
 }
