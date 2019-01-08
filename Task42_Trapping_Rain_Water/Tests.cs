b94e4a305ae9b722019e653bdbcb765d4c125c07
Michael Naumov <michael.naumov@volia-software.com>
2019-01-08 18:25:58 +0200

    42 Add failing test

--- Task42_Trapping_Rain_Water/Tests.cs
+++ Task42_Trapping_Rain_Water/Tests.cs
@@ -9,5 +9,11 @@ namespace AlgoSolving.Task42_Trapping_Rain_Water
         {
             Assert.That(new Solution().Trap(new[] { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 }), Is.EqualTo(6));
         }
+
+        [Test]
+        public void AcceptanceTest2()
+        {
+            Assert.That(new Solution().Trap(new[] { 2, 0, 2, 0, 1, 0 }), Is.EqualTo(3));
+        }
     }
 }
