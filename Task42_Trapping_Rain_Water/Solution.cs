ca522a18ede4f1009fed8598e5079bdbb30c90e3
Michael Naumov <michael.naumov@volia-software.com>
2019-01-08 17:23:43 +0200

    42 Implement

--- Task42_Trapping_Rain_Water/Solution.cs
+++ Task42_Trapping_Rain_Water/Solution.cs
@@ -4,7 +4,36 @@
     {
         public int Trap(int[] height)
         {
-            return 0;
+            var result = 0;
+            var leftIndex = 0;
+            var rightIndex = height.Length - 1;
+            var maxLeftHeight = 0;
+            var maxRightHeight = 0;
+
+            while (leftIndex < rightIndex)
+            {
+                if (height[leftIndex] >= maxLeftHeight)
+                {
+                    maxLeftHeight = height[leftIndex];
+                    leftIndex++;
+                }
+                else if (height[rightIndex] >= maxRightHeight)
+                {
+                    maxRightHeight = height[rightIndex];
+                    rightIndex--;
+                }
+                else
+                {
+                    if (maxLeftHeight <= maxRightHeight)
+                    {
+                        result += maxLeftHeight - height[leftIndex];
+                    }
+
+                    leftIndex++;
+                }
+            }
+
+            return result;
         }
     }
 }
