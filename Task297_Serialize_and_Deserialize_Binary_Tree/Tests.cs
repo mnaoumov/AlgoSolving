8b1dc02a0b7f904bf06d092abd0e7e1ba07b53d2
Michael Naumov <michael.naumov@volia-software.com>
2019-01-08 05:24:15 +0200

    297 Add test

---
+++ Task297_Serialize_and_Deserialize_Binary_Tree/Tests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task297_Serialize_and_Deserialize_Binary_Tree
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            TreeNode root = null;
+            var serialized = new Codec().serialize(root);
+            var deserialized = new Codec().deserialize(serialized);
+            Assert.That(deserialized, Is.EqualTo(root).Using(new TreeNodeEqualityComparer()));
+        }
+    }
+}
---
+++ Task297_Serialize_and_Deserialize_Binary_Tree/TreeNodeEqualityComparer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoSolving.Task297_Serialize_and_Deserialize_Binary_Tree
+{
+    internal class TreeNodeEqualityComparer : IEqualityComparer<TreeNode>
+    {
+        public bool Equals(TreeNode x, TreeNode y)
+        {
+            if (ReferenceEquals(x, y))
+            {
+                return true;
+            }
+
+            if (ReferenceEquals(x, null) || ReferenceEquals(y, null))
+            {
+                return false;
+            }
+
+            return x.val == y.val && Equals(x.left, y.left) && Equals(x.right, y.right);
+        }
+
+        public int GetHashCode(TreeNode obj)
+        {
+            if (ReferenceEquals(obj, null))
+            {
+                return 0;
+            }
+
+            return (obj.val, GetHashCode(obj.left), GetHashCode(obj.right)).GetHashCode();
+        }
+    }
+}
