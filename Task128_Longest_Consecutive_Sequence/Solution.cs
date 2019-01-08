409cc925d4620567fec6de6df2bd8a8390be88f6
Michael Naumov <michael.naumov@volia-software.com>
2019-01-08 04:11:56 +0200

    128 Add template

---
+++ Task128_Longest_Consecutive_Sequence/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task128_Longest_Consecutive_Sequence
+{
+    public class Solution
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Task128_Longest_Consecutive_Sequence/Tests.cs
@@ -0,0 +1,13 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task128_Longest_Consecutive_Sequence
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest()
+        {
+            Assert.That(new Solution().LongestConsecutive(new[] { 100, 4, 200, 1, 3, 2 }), Is.EqualTo(4));
+        }
+    }
+}
