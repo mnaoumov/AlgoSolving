e08eb09777598799056dd37a3590fc9ac82e290a
Michael Naumov <michael.naumov@volia-software.com>
2019-01-08 04:23:18 +0200

    128 Implement

--- Task128_Longest_Consecutive_Sequence/Solution.cs
+++ Task128_Longest_Consecutive_Sequence/Solution.cs
@@ -1,10 +1,41 @@
-﻿namespace AlgoSolving.Task128_Longest_Consecutive_Sequence
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoSolving.Task128_Longest_Consecutive_Sequence
 {
     public class Solution
     {
         public int LongestConsecutive(int[] nums)
         {
-            return 0;
+            var result = 0;
+
+            var ranges = new Dictionary<int, (int left, int right)>();
+
+            foreach (var num in nums)
+            {
+                if (ranges.ContainsKey(num))
+                {
+                    continue;
+                }
+
+                var left = num;
+                var right = num;
+
+                if (ranges.ContainsKey(num + 1))
+                {
+                    right = ranges[num + 1].right;
+                }
+
+                if (ranges.ContainsKey(num - 1))
+                {
+                    left = ranges[num - 1].left;
+                }
+
+                ranges[num] = (left, right);
+                result = Math.Max(result, right - left + 1);
+            }
+
+            return result;
         }
     }
 }
