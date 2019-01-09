fb5d91b5114f5323df4f8a9cdad81d6a657342c9
Michael Naumov <michael.naumov@volia-software.com>
2019-01-09 06:38:48 +0200

    15 Implement

--- Task15_3Sum/Solution.cs
+++ Task15_3Sum/Solution.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace AlgoSolving.Task15_3Sum
 {
@@ -6,7 +7,35 @@ namespace AlgoSolving.Task15_3Sum
     {
         public IList<IList<int>> ThreeSum(int[] nums)
         {
-            return null;
+            var set = new HashSet<int>(nums);
+            var keys = new HashSet<string>();
+
+            var results = new List<IList<int>>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    var first = nums[i];
+                    var second = nums[j];
+                    var third = -(first + second);
+                    if (set.Contains(third))
+                    {
+                        var triplet = new[] { first, second, third };
+                        Array.Sort(triplet);
+                        var key = string.Join(",", triplet);
+
+                        if (!keys.Contains(key))
+                        {
+                            results.Add(triplet);
+                            keys.Add(key);
+                        }
+
+                    }
+                }
+            }
+
+            return results;
         }
     }
 }
