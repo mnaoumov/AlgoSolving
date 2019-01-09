1b81432024e89af3c7cc7b89baa608200cc8b497
Michael Naumov <michael.naumov@volia-software.com>
2019-01-09 19:33:38 +0200

    239 Add template

---
+++ Task239_Sliding_Window_Maximum/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task239_Sliding_Window_Maximum
+{
+    public class Solution
+    {
+        public int[] MaxSlidingWindow(int[] nums, int k)
+        {
+            return new int[] { };
+        }
+    }
+}
---
+++ Task239_Sliding_Window_Maximum/Tests.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task239_Sliding_Window_Maximum
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            Assert.That(new Solution().MaxSlidingWindow(new[] { 1, 3, -1, -3, 5, 3, 6, 7 }, 3),
+                Is.EqualTo(new[] { 3, 3, 5, 5, 6, 7 }));
+        }
+    }
+}
