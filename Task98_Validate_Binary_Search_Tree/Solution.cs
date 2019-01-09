75287d60c9bad042821ecc65f489b6165eb29dd5
Michael Naumov <michael.naumov@volia-software.com>
2019-01-09 19:20:21 +0200

    98 Implement

--- Task98_Validate_Binary_Search_Tree/Solution.cs
+++ Task98_Validate_Binary_Search_Tree/Solution.cs
@@ -4,7 +4,19 @@
     {
         public bool IsValidBST(TreeNode root)
         {
-            return false;
+            return IsValidBST(root, int.MinValue, int.MaxValue);
+        }
+
+        private bool IsValidBST(TreeNode root, int min, int max)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+
+            return min <= root.val && root.val <= max &&
+                   IsValidBST(root.left, min, root.val) &&
+                   IsValidBST(root.right, root.val, max);
         }
     }
 }
