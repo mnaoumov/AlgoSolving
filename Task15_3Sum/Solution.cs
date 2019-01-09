373660d7d3a54fb9edda95175972cc99b26161dc
Michael Naumov <michael.naumov@volia-software.com>
2019-01-09 06:23:04 +0200

    15 Add template

---
+++ Task15_3Sum/Solution.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AlgoSolving.Task15_3Sum
+{
+    public class Solution
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            return null;
+        }
+    }
+}
---
+++ Task15_3Sum/Tests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AlgoSolving.Task15_3Sum
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            Assert.That(new Solution().ThreeSum(new[] { -1, 0, 1, 2, -1, -4 }), Is.EquivalentTo(new[]
+            {
+                new[] { -1, 0, 1 },
+                new[] { -1, -1, 2 }
+            }).Using<IList<int>>((ints1, ints2) => ints1.Count == ints2.Count && !ints1.Except(ints2).Any()));
+        }
+    }
+}
