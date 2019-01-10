249ea52a2e06c29104d775a4ad1b1de0ec2b84ac
Michael Naumov <michael.naumov@volia-software.com>
2019-01-10 18:00:08 +0200

    84 Implement

--- Task0084_Largest_Rectangle_in_Histogram/Solution.cs
+++ Task0084_Largest_Rectangle_in_Histogram/Solution.cs
@@ -1,10 +1,31 @@
-﻿namespace AlgoSolving.Task0084_Largest_Rectangle_in_Histogram
+﻿using System;
+
+namespace AlgoSolving.Task0084_Largest_Rectangle_in_Histogram
 {
     public class Solution
     {
         public int LargestRectangleArea(int[] heights)
         {
-            return 0;
+            var result = 0;
+
+            foreach (var height in heights)
+            {
+                var sequentialBarsNotBelowCurrent = 0;
+                foreach (var height2 in heights)
+                {
+                    if (height2 >= height)
+                    {
+                        sequentialBarsNotBelowCurrent++;
+                        result = Math.Max(result, sequentialBarsNotBelowCurrent * height);
+                    }
+                    else
+                    {
+                        sequentialBarsNotBelowCurrent = 0;
+                    }
+                }
+            }
+
+            return result;
         }
     }
 }
