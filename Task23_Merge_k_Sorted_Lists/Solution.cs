c24b77e37f3822ec9d7882cae6ee24a74ddce44d
Michael Naumov <michael.naumov@volia-software.com>
2019-01-10 04:38:56 +0200

    23 Implement

--- Task23_Merge_k_Sorted_Lists/Solution.cs
+++ Task23_Merge_k_Sorted_Lists/Solution.cs
@@ -4,7 +4,40 @@
     {
         public ListNode MergeKLists(ListNode[] lists)
         {
-            return null;
+            ListNode fakeRoot = new ListNode(0);
+            ListNode mergedListNode = fakeRoot;
+
+            while (true)
+            {
+                int min = int.MaxValue;
+                int minListIndex = -1;
+                var allListsEnded = true;
+                for (int i = 0; i < lists.Length; i++)
+                {
+                    if (lists[i] != null)
+                    {
+                        allListsEnded = false;
+                        if (lists[i].val <= min)
+                        {
+                            min = lists[i].val;
+                            minListIndex = i;
+                        }
+                    }
+                }
+
+                if (!allListsEnded)
+                {
+                    mergedListNode.next = new ListNode(min);
+                    mergedListNode = mergedListNode.next;
+                    lists[minListIndex] = lists[minListIndex].next;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return fakeRoot.next;
         }
     }
 }
