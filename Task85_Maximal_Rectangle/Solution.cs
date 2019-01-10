e3b4b8d8be1381c6aa1f333b3696c7eb29aae4ad
Michael Naumov <michael.naumov@volia-software.com>
2019-01-10 04:51:45 +0200

    85 Add template

---
+++ Task85_Maximal_Rectangle/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task85_Maximal_Rectangle
+{
+    public class Solution
+    {
+        public int MaximalRectangle(char[,] matrix)
+        {
+            return 0;
+        }
+    }
+}
