a587d879dc91bf04a88318cb8ea846c81bf5424e
Michael Naumov <michael.naumov@volia-software.com>
2019-01-10 22:59:05 +0200

    967 Implement

--- Task0967_Numbers_With_Same_Consecutive_Differences/Solution.cs
+++ Task0967_Numbers_With_Same_Consecutive_Differences/Solution.cs
@@ -1,4 +1,7 @@
-﻿namespace AlgoSolving.Task0967_Numbers_With_Same_Consecutive_Differences
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoSolving.Task0967_Numbers_With_Same_Consecutive_Differences
 {
     public class Solution
     {
@@ -6,7 +9,28 @@
         public int[] NumsSameConsecDiff(int N, int K)
         // ReSharper restore InconsistentNaming
         {
-            return new int[] { };
+            var results = Enumerable.Range(1, 9).ToList();
+            for (int i = 1; i < N; i++)
+            {
+                var nextResults = new List<int>();
+                foreach (var result in results)
+                {
+                    var digit = result % 10;
+                    if (digit >= K)
+                    {
+                        nextResults.Add(10 * result + digit - K);
+                    }
+
+                    if (digit <= 9 - K)
+                    {
+                        nextResults.Add(10*result + digit + K);
+                    }
+                }
+
+                results = nextResults;
+            }
+
+            return results.ToArray();
         }
     }
 }
