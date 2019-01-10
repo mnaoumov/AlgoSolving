cd538a2385df33765b60e559a35d29826aaf792b
Michael Naumov <michael.naumov@volia-software.com>
2019-01-11 00:41:35 +0200

    32 Implement

--- Task0032_Longest_Valid_Parentheses/Solution.cs
+++ Task0032_Longest_Valid_Parentheses/Solution.cs
@@ -1,10 +1,41 @@
-﻿namespace AlgoSolving.Task0032_Longest_Valid_Parentheses
+﻿using System;
+
+namespace AlgoSolving.Task0032_Longest_Valid_Parentheses
 {
     public class Solution
     {
         public int LongestValidParentheses(string s)
         {
-            return 0;
+            var result = 0;
+            var n = s.Length;
+            var longestPrefixes = new int[n + 1];
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                const char closeBracket = ')';
+                var endOfPreviousLongest = i + 1 + longestPrefixes[i + 1];
+
+                if (s[i] == closeBracket)
+                {
+                    longestPrefixes[i] = 0;
+                }
+                else if (s[i + 1] == closeBracket)
+                {
+                    longestPrefixes[i] = 2 + longestPrefixes[i + 2];
+                }
+                else if (endOfPreviousLongest < s.Length && s[endOfPreviousLongest] == closeBracket)
+                {
+                    longestPrefixes[i] = 2 + longestPrefixes[i + 1];
+                }
+                else
+                {
+                    longestPrefixes[i] = 0;
+                }
+
+                result = Math.Max(result, longestPrefixes[i]);
+            }
+
+            return result;
         }
     }
 }
