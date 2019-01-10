754632777a8db6adf6f1afbf8aabbb5351e821b3
Michael Naumov <michael.naumov@volia-software.com>
2019-01-11 01:19:27 +0200

    146 Implement

--- Task0146_LRU_Cache/LRUCache.cs
+++ Task0146_LRU_Cache/LRUCache.cs
@@ -1,18 +1,52 @@
-﻿namespace AlgoSolving.Task0146_LRU_Cache
+﻿using System.Collections.Generic;
+
+namespace AlgoSolving.Task0146_LRU_Cache
 {
     public class LRUCache
     {
+        private readonly int _capacity;
+        private readonly Dictionary<int, (int value, int time)> _dictionary;
+        private readonly SortedList<int, int> _timeToKeys;
+        private int _time;
+
         public LRUCache(int capacity)
         {
+            _capacity = capacity;
+            _dictionary = new Dictionary<int, (int value, int time)>(capacity);
+            _timeToKeys = new SortedList<int, int>(capacity);
+            _time = 0;
         }
 
         public int Get(int key)
         {
-            return 0;
+            const int notFound = -1;
+
+            _time++;
+
+            if (_dictionary.ContainsKey(key))
+            {
+                var entry = _dictionary[key];
+                _dictionary[key] = (entry.value, _time);
+                _timeToKeys.Remove(entry.time);
+                _timeToKeys.Add(_time, key);
+                return entry.value;
+            }
+
+            return notFound;
         }
 
         public void Put(int key, int value)
         {
+            _time++;
+            if (!_dictionary.ContainsKey(key) && _dictionary.Keys.Count == _capacity)
+            {
+                var leastUsedKey = _timeToKeys.Values[0];
+                _dictionary.Remove(leastUsedKey);
+                _timeToKeys.RemoveAt(0);
+
+            }
+            _dictionary[key] = (value, _time);
+            _timeToKeys.Add(_time, key);
         }
     }
 }
