66532a807a0a55c337968a7e9cd5740feef00822
Michael Naumov <michael.naumov@volia-software.com>
2019-01-10 21:12:29 +0200

    0076 Add failing test Letters_count_matters

--- Task0076_Minimum_Window_Substring/Tests.cs
+++ Task0076_Minimum_Window_Substring/Tests.cs
@@ -9,5 +9,11 @@ namespace AlgoSolving.Task0076_Minimum_Window_Substring
         {
             Assert.That(new Solution().MinWindow("ADOBECODEBANC", "ABC"), Is.EqualTo("BANC"));
         }
+
+        [Test]
+        public void Letters_count_matters()
+        {
+            Assert.That(new Solution().MinWindow("a", "aa"), Is.EqualTo(""));
+        }
     }
 }
