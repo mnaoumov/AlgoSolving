52b2d04dbc5db81d2e61ff81f3735f47bdaa700b
Michael Naumov <michael.naumov@volia-software.com>
2019-01-10 21:05:33 +0200

    76 Implement

--- Task0076_Minimum_Window_Substring/Solution.cs
+++ Task0076_Minimum_Window_Substring/Solution.cs
@@ -1,10 +1,94 @@
-﻿namespace AlgoSolving.Task0076_Minimum_Window_Substring
+﻿using System.Collections.Generic;
+
+namespace AlgoSolving.Task0076_Minimum_Window_Substring
 {
     public class Solution
     {
         public string MinWindow(string s, string t)
         {
-            return null;
+            if (t == "")
+            {
+                return "";
+            }
+
+            var symbolCounts = new Dictionary<char, int>();
+            foreach (var symbol in t)
+            {
+                symbolCounts[symbol] = 0;
+            }
+
+            var missingSymbolsCount = symbolCounts.Count;
+            var startIndex = 0;
+            const int notFound = -1;
+            var minWindowStartIndex = notFound;
+            var minWindowLength = int.MaxValue;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                var symbol = s[i];
+                if (!symbolCounts.ContainsKey(symbol))
+                {
+                    startIndex++;
+                    continue;
+                }
+
+                symbolCounts[symbol]++;
+
+                if (symbolCounts[symbol] == 1)
+                {
+                    missingSymbolsCount--;
+                }
+
+                if (missingSymbolsCount == 0)
+                {
+                    while (true)
+                    {
+                        var startSymbol = s[startIndex];
+                        if (symbolCounts.ContainsKey(startSymbol))
+                        {
+                            if (symbolCounts[startSymbol] == 1)
+                            {
+                                break;
+                            }
+
+                            symbolCounts[startSymbol]--;
+                        }
+
+                        startIndex++;
+                    }
+                }
+
+                var length = i - startIndex + 1;
+                if (length < minWindowLength && missingSymbolsCount == 0)
+                {
+                    minWindowLength = length;
+                    minWindowStartIndex = startIndex;
+                }
+
+                while (length >= minWindowLength)
+                {
+                    var startSymbol = s[startIndex];
+                    if (symbolCounts.ContainsKey(startSymbol))
+                    {
+                        if (symbolCounts[startSymbol] == 1)
+                        {
+                            missingSymbolsCount++;
+                        }
+
+                        symbolCounts[startSymbol]--;
+                    }
+
+                    startIndex++;
+                    length--;
+                }
+            }
+
+            if (minWindowStartIndex == notFound)
+            {
+                return "";
+            }
+            
+            return s.Substring(minWindowStartIndex, minWindowLength);
         }
     }
 }
