3f52bcf236762d00d7a957f76189e2987e2fac5a
Michael Naumov <michael.naumov@volia-software.com>
2019-01-11 00:02:06 +0200

    10 Implement

--- Task0010_Regular_Expression_Matching/Solution.cs
+++ Task0010_Regular_Expression_Matching/Solution.cs
@@ -4,6 +4,37 @@
     {
         public bool IsMatch(string s, string p)
         {
+            return IsMatch(s, p, 0, 0);
+        }
+
+        private bool IsMatch(string s, string p, int sIndex, int pIndex)
+        {
+            if (sIndex == s.Length || pIndex == p.Length)
+            {
+                return sIndex == s.Length && pIndex == p.Length;
+            }
+
+            const char anyModifier = '.';
+            const char multipleModifier = '*';
+
+            var patternSymbol = p[pIndex];
+            var hasMultipleModifier = pIndex + 1 < p.Length && p[pIndex + 1] == multipleModifier;
+
+            if (!hasMultipleModifier)
+            {
+                return (patternSymbol == anyModifier || patternSymbol == s[sIndex]) &&
+                       IsMatch(s, p, sIndex + 1, pIndex + 1);
+            }
+
+            do
+            {
+                if (IsMatch(s, p, sIndex, pIndex + 2))
+                {
+                    return true;
+                }
+                sIndex++;
+            } while (patternSymbol == anyModifier || (sIndex < s.Length && patternSymbol == s[sIndex]));
+
             return false;
         }
     }
