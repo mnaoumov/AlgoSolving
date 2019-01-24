b1ee33f6390d6fd6579ba78a560ba9f59114aa60
Michael Naumov <michael.naumov@volia-software.com>
2019-01-24 07:25:20 +0200

    124 Implement

--- Task0124_Binary_Tree_Maximum_Path_Sum/Solution.cs
+++ Task0124_Binary_Tree_Maximum_Path_Sum/Solution.cs
@@ -1,10 +1,50 @@
-﻿namespace AlgoSolving.Task0124_Binary_Tree_Maximum_Path_Sum
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AlgoSolving.Task0124_Binary_Tree_Maximum_Path_Sum
 {
     public class Solution
     {
         public int MaxPathSum(TreeNode root)
         {
-            return 0;
+            var maxPathSumDowns = new Dictionary<TreeNode, int>();
+            return MaxPathSum(root, maxPathSumDowns);
+        }
+
+        private int MaxPathSum(TreeNode root, Dictionary<TreeNode, int> maxPathSumDowns)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            return new[]
+            {
+                root.val + Math.Max(MaxPathSumDown(root.left, maxPathSumDowns), 0) + Math.Max(MaxPathSumDown(root.right, maxPathSumDowns), 0),
+                MaxPathSum(root.left, maxPathSumDowns),
+                MaxPathSum(root.right, maxPathSumDowns)
+            }.Max();
+        }
+
+        private int MaxPathSumDown(TreeNode root, Dictionary<TreeNode, int> maxPathSumDowns)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (!maxPathSumDowns.ContainsKey(root))
+            {
+                maxPathSumDowns[root] = root.val + new[]
+                {
+                    0,
+                    MaxPathSumDown(root.left, maxPathSumDowns),
+                    MaxPathSumDown(root.right, maxPathSumDowns)
+                }.Max();
+            }
+
+            return maxPathSumDowns[root];
         }
     }
 }
