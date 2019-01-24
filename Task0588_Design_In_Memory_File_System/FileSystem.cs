650503f16d41b9be7e9a344379da4ddb1ba346f7
Michael Naumov <michael.naumov@volia-software.com>
2019-01-25 01:38:17 +0200

    588 Add template

---
+++ Task0588_Design_In_Memory_File_System/FileSystem.cs
@@ -0,0 +1,23 @@
+﻿namespace AlgoSolving.Task0588_Design_In_Memory_File_System
+{
+    public class FileSystem
+    {
+        public string[] ls(string path)
+        {
+            return null;
+        }
+
+        public void mkdir(string path)
+        {
+        }
+
+        public void addContentToFile(string path, string content)
+        {
+        }
+
+        public string readContentFromFile(string path)
+        {
+            return null;
+        }
+    }
+}
---
+++ Task0588_Design_In_Memory_File_System/Tests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task0588_Design_In_Memory_File_System
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            var fs = new FileSystem();
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(fs.ls("/"), Is.EqualTo(new string[] { }));
+                fs.mkdir("/a/b/c");
+                fs.addContentToFile("/a/b/c/d", "hello");
+                Assert.That(fs.ls("/"), Is.EqualTo(new[] { "a" }));
+                Assert.That(fs.readContentFromFile("/a/b/c/d"), Is.EqualTo("hello"));
+            });
+        }
+    }
+}
