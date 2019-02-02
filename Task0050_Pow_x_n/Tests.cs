a2f3af2be4b558dabd045f04b0fa21919b8d991c
Michael Naumov <michael.naumov@volia-software.com>
2019-02-02 10:18:13 +0200

    50 Add equals test with Tolerance

--- Task0050_Pow_x_n/Tests.cs
+++ Task0050_Pow_x_n/Tests.cs
@@ -4,50 +4,52 @@ namespace AlgoSolving.Task0050_Pow_x_n
 {
     public class Tests
     {
+        private const double Tolerance = 1e-14;
+
         [Test]
         public void AcceptanceTest1()
         {
-            Assert.That(new Solution().MyPow(2.0, 10), Is.EqualTo(1024.0));
+            Assert.That(new Solution().MyPow(2.0, 10), Is.EqualTo(1024.0).Within(Tolerance));
         }
 
         [Test]
         public void AcceptanceTest2()
         {
-            Assert.That(new Solution().MyPow(2.1, 3), Is.EqualTo(9.261));
+            Assert.That(new Solution().MyPow(2.1, 3), Is.EqualTo(9.261).Within(Tolerance));
         }
 
         [Test]
         public void AcceptanceTest3()
         {
-            Assert.That(new Solution().MyPow(2.0, -2), Is.EqualTo(0.25));
+            Assert.That(new Solution().MyPow(2.0, -2), Is.EqualTo(0.25).Within(Tolerance));
         }
 
         [Test]
         [Timeout(1000)]
         public void Underflow()
         {
-            Assert.That(new Solution().MyPow(0.00001, int.MaxValue), Is.EqualTo(0.0));
+            Assert.That(new Solution().MyPow(0.00001, int.MaxValue), Is.EqualTo(0.0).Within(Tolerance));
         }
 
         [Test]
         [Timeout(1000)]
         public void One()
         {
-            Assert.That(new Solution().MyPow(1.0, int.MaxValue), Is.EqualTo(1.0));
+            Assert.That(new Solution().MyPow(1.0, int.MaxValue), Is.EqualTo(1.0).Within(Tolerance));
         }
 
         [Test]
         [Timeout(1000)]
         public void MinValue_power()
         {
-            Assert.That(new Solution().MyPow(2.0, int.MinValue), Is.EqualTo(0.0));
+            Assert.That(new Solution().MyPow(2.0, int.MinValue), Is.EqualTo(0.0).Within(Tolerance));
         }
 
         [Test]
         [Timeout(1000)]
         public void MinusOne()
         {
-            Assert.That(new Solution().MyPow(-1.0, int.MaxValue), Is.EqualTo(-1.0));
+            Assert.That(new Solution().MyPow(-1.0, int.MaxValue), Is.EqualTo(-1.0).Within(Tolerance));
         }
     }
 }
