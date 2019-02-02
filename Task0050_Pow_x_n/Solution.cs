6ad4839007d969730f46416094dbf41d596270c0
Michael Naumov <michael.naumov@volia-software.com>
2019-02-02 09:08:48 +0200

    50 Add template

---
+++ Task0050_Pow_x_n/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task0050_Pow_x_n
+{
+    public class Solution
+    {
+        public double MyPow(double x, int n)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Task0050_Pow_x_n/Tests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task0050_Pow_x_n
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            Assert.That(new Solution().MyPow(2.0, 10), Is.EqualTo(1024.0));
+        }
+
+        [Test]
+        public void AcceptanceTest2()
+        {
+            Assert.That(new Solution().MyPow(2.1, 3), Is.EqualTo(9.261));
+        }
+
+        [Test]
+        public void AcceptanceTest3()
+        {
+            Assert.That(new Solution().MyPow(2.0, -2), Is.EqualTo(0.25));
+        }
+    }
+}
