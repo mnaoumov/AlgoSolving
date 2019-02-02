f6a03d96791e15805c7490bac213795045a84964
Michael Naumov <michael.naumov@volia-software.com>
2019-02-02 10:25:06 +0200

    289 Add template

---
+++ Task0289_Game_of_Life/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task0289_Game_of_Life
+{
+    public class Solution
+    {
+        public void GameOfLife(int[][] board)
+        {
+
+        }
+    }
+}
---
+++ Task0289_Game_of_Life/Tests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task0289_Game_of_Life
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            var board = new[]
+            {
+                new[] { 0, 1, 0 },
+                new[] { 0, 0, 1 },
+                new[] { 1, 1, 1 },
+                new[] { 0, 0, 0 }
+            };
+            new Solution().GameOfLife(board);
+
+            Assert.That(board, Is.EqualTo(new[]
+            {
+                new[] { 0, 0, 0 },
+                new[] { 1, 0, 1 },
+                new[] { 0, 1, 1 },
+                new[] { 0, 1, 0 }
+            }));
+        }
+    }
+}
