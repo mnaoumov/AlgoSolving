776bb2172d03a44dd8bcbd52cc109a46d10e0611
Michael Naumov <michael.naumov@volia-software.com>
2019-02-02 09:40:16 +0200

    50 Switch to use double constant

--- Task0050_Pow_x_n/Solution.cs
+++ Task0050_Pow_x_n/Solution.cs
@@ -8,7 +8,7 @@ namespace AlgoSolving.Task0050_Pow_x_n
         {
             if (n == 0 || Math.Abs(x - 1.0) < double.Epsilon)
             {
-                return 1;
+                return 1.0;
             }
 
             if (n == int.MinValue)
