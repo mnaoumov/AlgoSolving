ffa236b353e71e65db3848c2f6f87b94468fa3ee
Michael Naumov <michael.naumov@volia-software.com>
2019-02-02 09:09:12 +0200

    50 Implement

--- Task0050_Pow_x_n/Solution.cs
+++ Task0050_Pow_x_n/Solution.cs
@@ -4,7 +4,23 @@
     {
         public double MyPow(double x, int n)
         {
-            return 0;
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            if (n < 0)
+            {
+                return MyPow(1 / x, -n);
+            }
+
+            var result = 1.0;
+            for (int i = 0; i < n; i++)
+            {
+                result *= x;
+            }
+
+            return result;
         }
     }
 }
