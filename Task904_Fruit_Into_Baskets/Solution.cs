e480c7cd573eccf4c4f6ffa35d2716742c489a98
Michael Naumov <michael.naumov@volia-software.com>
2019-02-02 16:39:49 +0200

    904 Implement

--- Task904_Fruit_Into_Baskets/Solution.cs
+++ Task904_Fruit_Into_Baskets/Solution.cs
@@ -1,10 +1,39 @@
-﻿namespace AlgoSolving.Task904_Fruit_Into_Baskets
+﻿using System;
+using System.Collections.Generic;
+
+namespace AlgoSolving.Task904_Fruit_Into_Baskets
 {
     public class Solution
     {
         public int TotalFruit(int[] tree)
         {
-            return 0;
+            const int basketCount = 2;
+            if (tree.Length <= basketCount)
+            {
+                return tree.Length;
+            }
+
+            var maxFruitCount = basketCount;
+
+            for (int i = 0; i < tree.Length; i++)
+            {
+                var fruitCount = 0;
+                var fruitTypes = new HashSet<int>();
+                for (int j = i; j < tree.Length; j++)
+                {
+                    fruitTypes.Add(tree[j]);
+                    if (fruitTypes.Count > basketCount)
+                    {
+                        break;
+                    }
+
+                    fruitCount++;
+                }
+
+                maxFruitCount = Math.Max(maxFruitCount, fruitCount);
+            }
+
+            return maxFruitCount;
         }
     }
 }
