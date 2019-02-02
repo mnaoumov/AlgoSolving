02d44cbeb1a0ec21f38c1131af8a83d284c494a7
Michael Naumov <michael.naumov@volia-software.com>
2019-02-02 16:22:25 +0200

    904 Add template

---
+++ Task904_Fruit_Into_Baskets/Solution.cs
@@ -0,0 +1,10 @@
+﻿namespace AlgoSolving.Task904_Fruit_Into_Baskets
+{
+    public class Solution
+    {
+        public int TotalFruit(int[] tree)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Task904_Fruit_Into_Baskets/Tests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace AlgoSolving.Task904_Fruit_Into_Baskets
+{
+    public class Tests
+    {
+        [Test]
+        public void AcceptanceTest1()
+        {
+            Assert.That(new Solution().TotalFruit(new[] { 1, 2, 1 }), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void AcceptanceTest2()
+        {
+            Assert.That(new Solution().TotalFruit(new[] { 0, 1, 2, 2 }), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void AcceptanceTest3()
+        {
+            Assert.That(new Solution().TotalFruit(new[] { 1, 2, 3, 2, 2 }), Is.EqualTo(4));
+        }
+
+        [Test]
+        public void AcceptanceTest4()
+        {
+            Assert.That(new Solution().TotalFruit(new[] { 3, 3, 3, 1, 2, 1, 1, 2, 3, 3, 4 }), Is.EqualTo(5));
+        }
+    }
+}
