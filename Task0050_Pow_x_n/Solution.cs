348ad97e88032e0dfbfe580325dae8b3f38b6771
Michael Naumov <michael.naumov@volia-software.com>
2019-02-02 10:10:58 +0200

    50 Rewrite to use recurse without decimal

--- Task0050_Pow_x_n/Solution.cs
+++ Task0050_Pow_x_n/Solution.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 
 namespace AlgoSolving.Task0050_Pow_x_n
 {
@@ -7,70 +6,30 @@ namespace AlgoSolving.Task0050_Pow_x_n
     {
         public double MyPow(double x, int n)
         {
-            return (double) MyPow((decimal) x, n);
-        }
-
-        private decimal MyPow(decimal x, int n)
-        {
-            if (n == 0 || x == 1m)
             {
-                return 1;
-            }
-
-            if (x == -1)
-            {
-                return n % 2 == 0 ? 1 : -1;
-            }
-
-            if (n == int.MinValue)
-            {
-                return MyPow(1 / x, int.MaxValue) / x;
-            }
-
-            if (n < 0)
-            {
-                return MyPow(1 / x, -n);
-            }
-
-            var powersOfTwo = new List<decimal> { x };
-
-            var power = 1;
-            int i = 1;
-
-            while (power <= n)
-            {
-                var last = powersOfTwo.Last();
-                if (last == 0)
+                if (n == 0 || Math.Abs(x - 1) < double.Epsilon)
                 {
-                    return 0;
+                    return 1;
                 }
 
-                powersOfTwo.Add(last * last);
-
-                if (powersOfTwo[i] == 0)
+                if (Math.Abs(x + 1) < double.Epsilon)
                 {
-                    return 0;
+                    return n % 2 == 0 ? 1 : -1;
                 }
 
-                power *= 2;
-                i++;
-            }
-
-            var result = 1m;
-            i = 0;
-
-            while (n > 0)
-            {
-                if (n % 2 == 1)
+                if (n == int.MinValue)
                 {
-                    result *= powersOfTwo[i];
+                    return 1 / MyPow(x, Int32.MaxValue) / x;
                 }
-                n /= 2;
-                i++;
+
+                if (n < 0)
+                {
+                    return 1 / MyPow(x, -n);
+                }
+
+                var y = MyPow(x, n / 2);
+                return y * y * (n % 2 == 0 ? 1 : x);
             }
-
-
-            return result;
         }
     }
 }
