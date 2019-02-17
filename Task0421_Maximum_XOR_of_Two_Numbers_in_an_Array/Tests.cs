63658b6658bbf5d4b0555ebef296fe48354ebf75
Michael Naumov <michael.naumov@volia-software.com>
2019-02-17 08:13:37 +0200

    421 Fix typo in test case

--- Task0421_Maximum_XOR_of_Two_Numbers_in_an_Array/Tests.cs
+++ Task0421_Maximum_XOR_of_Two_Numbers_in_an_Array/Tests.cs
@@ -7,7 +7,7 @@ namespace AlgoSolving.Task0421_Maximum_XOR_of_Two_Numbers_in_an_Array
         [Test]
         public void AcceptanceTest1()
         {
-            Assert.That(new Solution().FindMaximumXOR(new[] { 3, 10, 5, 25, 2, 8 }), Is.EqualTo(20));
+            Assert.That(new Solution().FindMaximumXOR(new[] { 3, 10, 5, 25, 2, 8 }), Is.EqualTo(28));
         }
     }
 }
