2154e87c248fda3101a28ac6771bb0c48f17eff5
Michael Naumov <michael.naumov@volia-software.com>
2019-02-17 08:13:58 +0200

    421 Implement

--- Task0421_Maximum_XOR_of_Two_Numbers_in_an_Array/Solution.cs
+++ Task0421_Maximum_XOR_of_Two_Numbers_in_an_Array/Solution.cs
@@ -1,12 +1,53 @@
-﻿namespace AlgoSolving.Task0421_Maximum_XOR_of_Two_Numbers_in_an_Array
+﻿using System;
+using System.Linq;
+
+namespace AlgoSolving.Task0421_Maximum_XOR_of_Two_Numbers_in_an_Array
 {
     public class Solution
     {
         // ReSharper disable once MemberCanBeMadeStatic.Global
-        // ReSharper disable once InconsistentNaming
         public int FindMaximumXOR(int[] nums)
         {
-            return 0;
+            const int maxPosition = 31;
+
+            return FindMaximumXOR(maxPosition, nums, nums);
+        }
+
+        private static int FindMaximumXOR(int position, int[] numbers1, int[] numbers2)
+        {
+            if (!numbers1.Any() || !numbers2.Any())
+            {
+                return 0;
+            }
+
+            if (position < 0)
+            {
+                return numbers1[0] ^ numbers2[0];
+            }
+
+            var numbers1WithOneBit = numbers1.Where(n => HasOneBit(n, position)).ToArray();
+            var numbers1WithZeroBit = numbers1.Where(n => !HasOneBit(n, position)).ToArray();
+            var numbers2WithOneBit = numbers2.Where(n => HasOneBit(n, position)).ToArray();
+            var numbers2WithZeroBit = numbers2.Where(n => !HasOneBit(n, position)).ToArray();
+
+            int result = FindMaximumXOR(position - 1, numbers1WithOneBit, numbers2WithZeroBit);
+
+            if (numbers1 != numbers2)
+            {
+                result = Math.Max(result, FindMaximumXOR(position - 1, numbers2WithOneBit, numbers1WithZeroBit));
+            }
+
+            if (result != 0)
+            {
+                return result;
+            }
+
+            return FindMaximumXOR(position - 1, numbers1, numbers2);
+        }
+
+        private static bool HasOneBit(int n, int position)
+        {
+            return ((n >> position) & 1) == 1;
         }
     }
 }
