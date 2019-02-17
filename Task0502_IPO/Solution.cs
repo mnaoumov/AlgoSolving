4dca734e3b4b80afc0fd0367ed7d1c46e9a4b431
Michael Naumov <michael.naumov@volia-software.com>
2019-02-17 08:35:12 +0200

    502 Implement

--- Task0502_IPO/Solution.cs
+++ Task0502_IPO/Solution.cs
@@ -1,4 +1,6 @@
-﻿namespace AlgoSolving.Task0502_IPO
+﻿using System.Collections.Generic;
+
+namespace AlgoSolving.Task0502_IPO
 {
     public class Solution
     {
@@ -6,7 +8,41 @@
         public int FindMaximizedCapital(int k, int W, int[] Profits, int[] Capital)
         // ReSharper restore InconsistentNaming
         {
-            return 0;
+            var companyCapital = W;
+            var n = Capital.Length;
+            var projectsTakenIndices = new HashSet<int>();
+
+            for (int i = 0; i < k; i++)
+            {
+                const int noProjectIndex = -1;
+                var bestProjectIndex = noProjectIndex;
+                var bestProfit = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    if (projectsTakenIndices.Contains(j) || Capital[j] > companyCapital)
+                    {
+                        continue;
+                    }
+
+                    var profit = Profits[j];
+
+                    if (profit > bestProfit)
+                    {
+                        bestProfit = profit;
+                        bestProjectIndex = j;
+                    }
+                }
+
+                if (bestProjectIndex == noProjectIndex)
+                {
+                    break;
+                }
+
+                projectsTakenIndices.Add(bestProjectIndex);
+                companyCapital += bestProfit;
+            }
+
+            return companyCapital;
         }
     }
 }
