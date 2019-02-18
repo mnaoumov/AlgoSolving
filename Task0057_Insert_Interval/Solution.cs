f895fe7dfd8796883acedbf21d51a51087515e32
Michael Naumov <michael.naumov@volia-software.com>
2019-02-18 11:01:14 +0200

    57 Implement

--- Task0057_Insert_Interval/Solution.cs
+++ Task0057_Insert_Interval/Solution.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AlgoSolving.Task0057_Insert_Interval
 {
@@ -6,7 +8,54 @@ namespace AlgoSolving.Task0057_Insert_Interval
     {
         public IList<Interval> Insert(IList<Interval> intervals, Interval newInterval)
         {
-            return null;
+            var result = intervals.ToList();
+
+            var leftIndex = 0;
+            var rightIndex = intervals.Count - 1;
+
+            while (leftIndex <= rightIndex)
+            {
+                var middleIndex = leftIndex + (rightIndex - leftIndex) / 2;
+                if (intervals[middleIndex].start >= newInterval.start)
+                {
+                    rightIndex = middleIndex - 1;
+                }
+                else
+                {
+                    leftIndex = middleIndex + 1;
+                }
+            }
+
+            var newIntervalIndex = leftIndex;
+
+            result.Insert(newIntervalIndex, newInterval);
+
+            if (newIntervalIndex - 1 >= 0)
+            {
+                var previousInterval = result[newIntervalIndex - 1];
+                if (previousInterval.end >= newInterval.start)
+                {
+                    newInterval.start = previousInterval.start;
+                    result.RemoveAt(newIntervalIndex - 1);
+                    newIntervalIndex--;
+                }
+            }
+
+            for (int i = newIntervalIndex + 1; i < result.Count; i++)
+            {
+                var nextInterval = result[newIntervalIndex + 1];
+                if (nextInterval.start <= newInterval.end)
+                {
+                    newInterval.end = Math.Max(newInterval.end, nextInterval.end);
+                    result.RemoveAt(newIntervalIndex + 1);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return result;
         }
     }
 }
