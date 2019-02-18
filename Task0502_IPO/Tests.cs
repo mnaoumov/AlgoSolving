2a3e31115aebc889c2868f40ccb39e5d1a542fee
Michael Naumov <michael.naumov@volia-software.com>
2019-02-18 09:25:45 +0200

    502 Add failing More_attempts_than_projects

--- Task0502_IPO/Tests.cs
+++ Task0502_IPO/Tests.cs
@@ -17,5 +17,11 @@ namespace AlgoSolving.Task0502_IPO
         {
             Assert.That(new Solution().FindMaximizedCapital(50000, 50000, Enumerable.Range(0, 50000).ToArray(), Enumerable.Range(0, 50000).ToArray()), Is.EqualTo(1250025000));
         }
+
+        [Test]
+        public void More_attempts_than_projects()
+        {
+            Assert.That(new Solution().FindMaximizedCapital(10, 0, new[] { 1, 2, 3 }, new[] { 0, 1, 2 }), Is.EqualTo(6));
+        }
     }
 }
