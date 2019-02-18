9ca290d0b2ab74e918d207e70f58e49639ad3c6f
Michael Naumov <michael.naumov@volia-software.com>
2019-02-18 11:03:34 +0200

    57 Add test comparison and formatting

--- Task0057_Insert_Interval/Tests.cs
+++ Task0057_Insert_Interval/Tests.cs
@@ -4,18 +4,34 @@ namespace AlgoSolving.Task0057_Insert_Interval
 {
     public class Tests
     {
+        [SetUp]
+        public void BeforeEachTests()
+        {
+            TestContext.AddFormatter<Interval>(val =>
+            {
+                var interval = (Interval)val;
+                return $"[{interval.start}, {interval.end}]";
+            });
+        }
+
         [Test]
         public void AcceptanceTest1()
         {
             Assert.That(new Solution().Insert(new[] { new Interval(1, 3), new Interval(6, 9) }, new Interval(2, 5)),
-                Is.EqualTo(new[] { new Interval(1, 5), new Interval(6, 9) }));
+                Is.EquivalentTo(new[] { new Interval(1, 5), new Interval(6, 9) }).Using<Interval, Interval>(IntervalComparer));
         }
 
         [Test]
         public void AcceptanceTest2()
         {
             Assert.That(new Solution().Insert(new[] { new Interval(1, 2), new Interval(3, 5), new Interval(6, 7), new Interval(8, 10), new Interval(12, 16) }, new Interval(4, 8)),
-                Is.EqualTo(new[] { new Interval(1, 2), new Interval(3, 10), new Interval(12, 16) }));
+                Is.EquivalentTo(new[] { new Interval(1, 2), new Interval(3, 10), new Interval(12, 16) }).Using<Interval, Interval>(IntervalComparer));
         }
+
+        private static bool IntervalComparer(Interval interval1, Interval interval2)
+        {
+            return interval1.start == interval2.start && interval1.end == interval2.end;
+        }
+
     }
 }
